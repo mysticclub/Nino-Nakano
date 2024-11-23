0333bfe07b11294ad4c2f27247dd8a7b6f0c7efb
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-23 16:59:05 -0400

    Actualizar downloader-ytmp4.js

--- plugins/downloader-ytmp4.js
+++ plugins/downloader-ytmp4.js
@@ -2,41 +2,6 @@
 - YTMP4 By Angel-OFC 
 - https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
 */
-/* import { ytmp4 } from 'ruhend-scraper';
-
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!args || !args[0]) return conn.reply(m.chat, '*\`Ingresa El link Del video a descargar 🤍\`*', m, fake)
-    await m.react('🕓');
-    try {
-        const { title, video, author, description, duration, views, upload, thumbnail } = await ytmp4(args[0]);
-
-        let caption = `🎬 *Título:* ${title}\n`;
-        caption += `👤 *Autor:* ${author}\n`;
-        caption += `📝 *Descripción:* ${description}\n`;
-        caption += `⏳ *Duración:* ${duration}\n`;
-        caption += `👁️ *Vistas:* ${views}\n`;
-        caption += `📅 *Subido:* ${upload}`;
-
-        await conn.sendMessage(m.chat, { 
-            video: { url: video }, 
-            caption: caption, 
-            mimetype: 'video/mp4' 
-        }, { quoted: m });
-        await m.react('✅'); 
-
-    } catch (error) {
-        console.error(error);
-        await m.react('✖️');
-    }
-};
-
-handler.help = ['video *<link yt>*'];
-handler.tags = ['dl'];
-handler.command = ['ytmp4', 'video', 'vídeo'];
-handler.register = true;
-
-export default handler; */
-
 import { ytmp4 } from 'ruhend-scraper';
 import fetch from 'node-fetch';
 
@@ -66,20 +31,23 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
     await m.react('🕓');
     try {
         let videoUrl = urls[0];
-        let { title, audio, author, description, duration, views, upload, thumbnail } = await ytmp4(videoUrl);
+        let { title, video, author, description, duration, views, upload, thumbnail } = await ytmp4(videoUrl);
 
-       let caption = `🎬 *Título:* ${title}\n`;
+        // Formatear el mensaje con los detalles del video
+        let caption = `🎬 *Título:* ${title}\n`;
         caption += `👤 *Autor:* ${author}\n`;
         caption += `📝 *Descripción:* ${description}\n`;
         caption += `⏳ *Duración:* ${duration}\n`;
         caption += `👁️ *Vistas:* ${views}\n`;
         caption += `📅 *Subido:* ${upload}`;
 
+        // Enviar el video al usuario
         await conn.sendMessage(m.chat, { 
             video: { url: video }, 
             caption: caption, 
             mimetype: 'video/mp4' 
         }, { quoted: m });
+
         await m.react('✅');
     } catch (e) {
         console.error(e);
