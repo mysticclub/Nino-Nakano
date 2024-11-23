cf3d0874cf77d3de84c90dc11c4748ab5a4f25be
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-23 17:47:26 -0400

    Actualizar downloader-ytmp3doc.js

--- plugins/downloader-ytmp3doc.js
+++ plugins/downloader-ytmp3doc.js
@@ -1,29 +1,25 @@
-/* 
-- YTMP3 By Angel-OFC 
-- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
-*/
-import { youtubedl } from '@bochilteam/scraper-youtube'
+import { youtubedl } from '@bochilteam/scraper-youtube';
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
     if (!m.quoted) {
-        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m, fake)
+        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m)
             .then(_ => m.react('✖️'));
     }
 
     if (!m.quoted.text.includes("*\`【Y O U T U B E - P L A Y】\`*")) {
-        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m, fake)
+        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m)
             .then(_ => m.react('✖️'));
     }
 
     let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'));
 
     if (!urls) {
-        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m, fake).then(_ => m.react('✖️'));
+        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m).then(_ => m.react('✖️'));
     }
 
     if (urls.length < parseInt(text)) {
-        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m, fake).then(_ => m.react('✖️'));
+        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m).then(_ => m.react('✖️'));
     }
 
     let user = global.db.data.users[m.sender];
@@ -33,17 +29,24 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
         let videoUrl = urls[0];
         let { title, description, viewH, duration, url } = await youtubedl(videoUrl);
 
-       await conn.sendMessage(m.chat, { document: { url: url }, caption: '*By: GenesisBot*', mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m })
+        // Enviar el archivo MP3 (documento)
+        await conn.sendMessage(m.chat, { 
+            document: { url: url }, 
+            caption: '*By: GenesisBot*', 
+            mimetype: 'audio/mpeg', 
+            fileName: `${title}.mp3`
+        }, { quoted: m });
+
         await m.react('✅');
     } catch (e) {
         console.error(e);
-        await conn.reply(m.chat, `*\`Hubo un error al procesar la descarga.🤍\`*`, m, fake).then(_ => m.react('✖️'));
+        await conn.reply(m.chat, `*\`Hubo un error al procesar la descarga.🤍\`*`, m).then(_ => m.react('✖️'));
     }
 };
 
 handler.help = ['Audio'];
 handler.tags = ['downloader'];
 handler.customPrefix = /^(Audio|audio)/;
-handler.command = new RegExp;
+handler.command = new RegExp();
 
 export default handler;
