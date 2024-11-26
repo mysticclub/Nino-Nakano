d44e5657f5d2bd18aaa48d5ca3e0dae4c1e38bf1
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-26 00:47:35 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -35,15 +35,15 @@ export async function before(m, { conn, groupMetadata }) {
 
       if (chat.bienvenida && m.messageStubType == 27) {
     let welcome = `*⭒─ׄ─ׅ─ׄ─⭒ \`ʙɪᴇɴᴠᴇɴɪᴅᴀ\` ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ᴡᴇʟᴄᴏᴍᴇ* :: @${m.messageStubParameters[0].split`@`[0]}\n┊:⁖֟⊱┈֟፝❥  ${groupMetadata.subject}\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`
-await conn.sendMini(m.chat, titulowm2, titu, welcome, img, img, canal, estilo)
+await conn.sendMini(m.chat, titulowm2, titu, welcome, filePath, filePath, canal, estilo)
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
     let bye = `*⭒─ׄ─ׅ─ׄ─⭒ \`ᴀ ᴅ ɪ ᴏ ꜱ\` ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ʙ ʏ ᴇ* :: @${m.messageStubParameters[0].split`@`[0]}\n┊:⁖֟⊱┈֟፝❥   *ꜱ ᴀ ʏ ᴏ ɴ ᴀ ʀ ᴀ 👋*\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`
-await conn.sendMini(m.chat, titulowm2, titu, bye, img, img, canal, estilo)
+await conn.sendMini(m.chat, titulowm2, titu, bye, filePath, filePath, canal, estilo)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
     let kick = `*⭒─ׄ─ׅ─ׄ─⭒ \`ᴀ ᴅ ɪ ᴏ ꜱ\` ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *ʙ ʏ ᴇ* :: @${m.messageStubParameters[0].split`@`[0]}\n┊:⁖֟⊱┈֟፝❥   *ꜱ ᴀ ʏ ᴏ ɴ ᴀ ʀ ᴀ 👋*\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`
-await conn.sendMini(m.chat, titulowm2, titu, kick, img, img, canal, estilo)
+await conn.sendMini(m.chat, titulowm2, titu, kick, filePath, filePath, canal, estilo)
 }}
