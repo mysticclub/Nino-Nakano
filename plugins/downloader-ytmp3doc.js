e279fd9808b794d09bafdc83b9c75a85b04d86f5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-26 01:49:55 -0400

    Actualizar downloader-ytmp3doc.js

--- plugins/downloader-ytmp3doc.js
+++ plugins/downloader-ytmp3doc.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
             mimetype: 'audio/mpeg', 
             fileName: `${title}.mp3`, 
             caption: `🎵 *Título:* ${title}\n👤 *Autor:* ${author}\n⏳ *Duración:* ${duration}\n👀 *Vistas:* ${views}`,
-            thumbnail: await (await fetch(thumbnail)).buffer()}}}, { quoted: m })
+            }, { quoted: m })
 
         await m.react('✅');
     } catch (e) {
