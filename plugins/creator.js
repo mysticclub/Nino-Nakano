7c9b11f701be262554f7a0aa0b58bbb80fb737b9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-27 22:54:12 -0400

    Actualizar creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,17 +1,21 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+m.react('🐈‍⬛')
+
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
+let edtr = `@${m.sender.split`@`[0]}`
 
-  const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `✧ Developer Bot `, `No famoso`, `m46234391@gmail.com`, `🇦🇷 Argentina`, `📍 https://github.com/MauroAzcurra`, `✧ Owner Waguri Ai`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `✧ Whatsapp Bot`, `✧ No hagas spam.`, `m46234391@gmail.com`, `🇦🇷 Argentina`, `📍 https://github.com/MauroAzcurra/Waguri-Ai`, `Si hay un error habla con mi owner ☺`]
-  ], fkontak)
-  await m.reply(`Hola @${m.sender.split(`@`)[0]} solo habla con mi Owner por temas del bot.`)
-  } 
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;  ૈANGELITO 🍃\nNICKNAME:🐈‍⬛ Angel\nORG: ૈANGELITO 🐈‍⬛\nTITLE:soft\nitem1.TEL;waid=59897246324:+598 97 246 324\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/karim-off/GenesisBot-Pro1\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: agasistencia2@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇦🇷 Argentina;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:🐈‍⬛ 09-12-2007\nEND:VCARD`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
+let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🐈‍⬛*`
+    await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
 
-handler.help = ['owner', 'creador']
-handler.tags = ['main', 'info']
-handler.command = /^(owner|creador)/i
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['main']
+handler.command = /^(owner|creator|creador|dueño)$/i
+
 export default handler
