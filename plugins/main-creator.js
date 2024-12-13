2df851da6e7c67f31866b215371a536a0cbf66e2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-13 16:26:13 -0400

    Delete plugins directory

--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let txt_owner = "> _*`Hola, Este es el numero de mi creadxr, cualquier falla o si quieres agregar el bot a tu grupo, puedes hablarle`*_\n\n `+54 9 381 645-7465`"
-await conn.sendFile(m.chat, "https://th.bing.com/th/id/OIP.UrDEsrIiX93efNEOpMTezgHaEo?dpr=2&pid=ImgDetMain", 'thumbnail.jpg', txt_owner, m, null, rcanal)
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
