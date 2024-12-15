cd90e80e3bffa3e47e6523673d2592673d4686ba
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-15 13:06:42 -0400

    Actualizar rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn }) => {
             currentLvl: user.level,
             currentRank: 0, // Puedes agregar clasificación si tienes un sistema
             currentXP: user.exp,
-            requiredXP: max,
+            requiredXP: xp,
             backgroundColor: { background: '#070d19', bubbles: '#0ca7ff' },
             avatarImgURL: 'https://telegra.ph/file/b97148e2154508f63d909.jpg', // Puedes usar otro avatar dinámico
             nicknameText: { content: name, font: 'Nunito', color: '#0CA7FF' },
