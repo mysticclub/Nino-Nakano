a3c36e9da3ec2a2bcf301456d6697e503e31411a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-15 01:41:41 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -298,12 +298,12 @@ process.on('uncaughtException', console.error)
 let isInit = true;
 let handler = await import('./handler.js')
 global.reloadHandler = async function(restatConn) {
-/*  try {
+  try {
     const Handler = await import(`./handler.js?update=${Date.now()}`).catch(console.error);
     if (Object.keys(Handler || {}).length) handler = Handler
   } catch (e) {
     console.error(e);
-  } */
+  }
   if (restatConn) {
     const oldChats = global.conn.chats
     try {
