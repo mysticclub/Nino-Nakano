662c86dc656b48b6c36eef9ba77aaa492ec24597
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-14 22:15:00 -0400

    Actualizar serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -18,7 +18,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
    let totalUsers = uniqueUsers.size
    let img = await (await fetch(`https://pomf2.lain.la/f/u0t0ek6.jpg`)).buffer()
   let message = users.map((v, index) => `
-*[ \`${index + 1}\` -  ${v.user.name || 'Sin Nombre'} ]*\n* *🤍 \` Link :\`* https://wa.me/${v.user.jid.replace(/[^0-9]/g , '')}\n`).join('\n\n')
+*[ \`${index + 1}\` -  ${v.user.name || 'Sin Nombre'} ]*\n* *🤍 \`Link :\`* https://wa.me/${v.user.jid.replace(/[^0-9]/g , '')}\n`).join('\n\n')
 
   let replyMessage = message.length === 0 ? '' : message
 
