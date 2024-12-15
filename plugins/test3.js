71ca2b89babc48ba29bdf48c6a1eaa166c705c6f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-14 23:35:18 -0400

    Actualizar test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,4 +1,55 @@
-let handler = async (m, { conn, participants }) => {
+import MessageType from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
+let handler = async (m, { conn, text, participants }) => {
+    // Filtrar los participantes, excluyendo al creador y al bot
+    const groupAdmins = participants.filter(p => p.admin);
+    const botId = conn.user.jid;
+    const groupOwner = groupAdmins.find(p => p.isAdmin)?.id;  // Encontrar el propietario del grupo
+    const groupNoAdmins = participants.filter(p => p.id !== botId && p.id !== groupOwner && !p.admin).map(p => p.id);
+
+    if (groupNoAdmins.length === 0) throw '*⚠️ No hay usuarios para eliminar.*'; // Verifica que haya usuarios para eliminar
+
+    // Si hay texto (como .kickall <texto>) lo reenviamos con las menciones
+    let users = participants.map(u => conn.decodeJid(u.id));
+    let pesan = text || 'Grupo limpiado por el bot';  // Mensaje por defecto
+
+    // Crear mensaje con menciones
+    const msg = conn.cMod(m.chat,
+        generateWAMessageFromContent(m.chat, {
+            [MessageType.extendedTextMessage]: {
+                text: pesan,
+                mentions: users
+            }
+        }, {
+            userJid: conn.user.id
+        }),
+        pesan, conn.user.jid, { mentions: users }
+    );
+
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+
+    // Eliminar a cada miembro con un retraso de 2 segundos
+    for (let userId of groupNoAdmins) {
+        await conn.groupParticipantsUpdate(m.chat, [userId], 'remove');
+        await new Promise(resolve => setTimeout(resolve, 2000));  // Espera de 2 segundos entre eliminaciones
+    }
+
+    m.reply('*[🌠] Eliminación Exitosa.*');
+}
+
+handler.help = ['kickall', '-'].map(v => 'o' + v + ' @user');
+handler.tags = ['owner'];
+handler.command = /^(kickall)$/i;
+
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
+
+
+
+/* let handler = async (m, { conn, participants }) => {
     // Filtrar los participantes, excluyendo al creador y al bot
     const groupAdmins = participants.filter(p => p.admin);
     const botId = conn.user.jid;
@@ -25,4 +76,4 @@ handler.command = /^(kickall)$/i;
 handler.group = true;
 handler.botAdmin = true;
 
-export default handler;
+export default handler; */
