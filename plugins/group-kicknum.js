9adeda88c6bf2b3bfbe358e4f5516260744b812b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-15 02:41:19 -0400

    Actualizar group-kicknum.js

--- plugins/group-kicknum.js
+++ plugins/group-kicknum.js
@@ -29,6 +29,8 @@ const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, 
       break;
   }
 };
+handler.help = ['listanum', 'kicknum']
+handler.tags = ['group'];
 handler.command = /^(listanum|kicknum|listnum)$/i;
 handler.group = handler.botAdmin = handler.admin = true;
 handler.fail = null;
