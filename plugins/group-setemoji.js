f1e215fcc7b52506ccd3a90a232f0325813bef74
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-14 22:59:54 -0400

    Actualizar group-setemoji.js

--- plugins/group-setemoji.js
+++ plugins/group-setemoji.js
@@ -28,7 +28,7 @@ let handler = async (m, { conn, text, isRowner }) => {
 const isEmoji = (text) => {
   const emojiRegex =
     /(?:\p{Emoji_Presentation}|\p{Extended_Pictographic}|\p{Emoji})/gu;
-  return emojiRegex.test(text) && text.length <= 2; // Permitir solo un emoji
+  return emojiRegex.test(text) && text.length <= 2;
 };
 
 handler.help = ['setemoji <emoji>'];
