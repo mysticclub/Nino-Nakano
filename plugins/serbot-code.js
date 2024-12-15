7d68bbe3a3cf820e9b4fe58ab19d0ae8fe406124
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-15 02:47:47 -0400

    Actualizar serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -86,7 +86,7 @@ if (methodCode && !conn.authState.creds.registered) {
         codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
         let txt = `*\`「🤍」 Serbot - Code 「🤍」\`*\n\n*\`[ Pasos : ]\`*\n\`1 ❥\` _Click en los 3 puntos_\n\`2 ❥\` _Toca en dispositivos vinculados_\n\`3 ❥\` _Seleciona Vincular con codigo_\n\`4 ❥\` _Escribe El Codigo_\n\n> *:⁖֟⊱┈֟፝❥ Nota:* Este Codigo Solo Funciona Con Quien Lo Solicito`
          await parent.reply(m.chat, txt, m, rcanal)
-         await parent.reply(m.chat, codeBot, m, rcanal)
+         await parent.reply(m.chat, codeBot, m, rpl)
         rl.close()
     }, 3000)
 }
