216a87325dc48c3ce2c848c7f489f6248c35af2f
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 14:55:50 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -3,40 +3,36 @@
 *❀ By JTxs*
 
 [ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ YTMP4 ]*
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n*/
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
+    if (!text) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Ingresa un link de youtube`, m, fake);
+    }
 
-try {
-let api = await fetch(`https://axeel.my.id/api/download/video?url=${text}`)
-let json = await api.json()
-let { title, views, likes, description, author } = json.metadata
-let HS = `- *Titulo :* ${title}
-- *Descripcion :* ${description}
-- *Visitas :* ${views}
-- *Likes :* ${likes}
-- *Autor :* ${author}
-- *Tamaño :* ${json.downloads.size}
-`
-await conn.sendFile(m.chat, json.downloads.url, 'HasumiBotFreeCodes.mp4', HS, m)
-} catch (error) {
-console.error(error)
-}}
+    try {
+        await m.react('🕒');
 
-handler.command = /^(ytmp4)$/i
+        let api = await fetch(`https://axeel.my.id/api/download/video?url=${text}`);
+        let json = await api.json();
+        let { title, views, likes, description, author } = json.metadata;
+        let txt = `• *Titulo :* ${title}
+• *Autor :* ${author}
+• *Tamaño :* ${json.downloads.size}`;
 
-export default handler
+        await conn.sendMessage(m.chat, { video: { url: json.downloads.url }, caption: txt }, { quoted: m });
+        await m.react('✅');
+    } catch (error) {
+        console.error(error);
+        await m.react('❌');
+        conn.reply(m.chat, `☁️ Hubo un error al procesar tu solicitud. Inténtalo de nuevo más tarde.`, m);
+    }
+};
+
+handler.help = ['ytmp4 *<url>*'];
+handler.tags = ['dl'];
+handler.command = /^(ytmp4)$/i;
+
+export default handler;
