84ba2cb6086e313f1c9526799745af0462929af9
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 14:55:33 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,43 +1,48 @@
-/* 
-
-*❀ By JTxs*
+/* *❀ By JTxs*
 
 [ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ YTMP3 ]*
-import fetch from 'node-fetch'
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n */
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
+    if (!text) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Ingresa un link de YouTube`, m, fake);
+    }
 
-try {
-let api = await fetch(`https://axeel.my.id/api/download/audio?url=${text}`)
-let json = await api.json()
-let { title, views, likes, description, author, thumbnail } = json.metadata
-let HS = `- *Titulo :* ${title}
-- *Descripcion :* ${description}
-- *Visitas :* ${views}
-- *Likes :* ${likes}
-- *Autor :* ${author}
-- *Tamaño :* ${json.downloads.size}
-`
-m.reply(HS)
-await conn.sendMessage(m.chat, { audio: { url: json.downloads.url }, mimetype: 'audio/mpeg' }, { quoted: m });
-} catch (error) {
-console.error(error)
-}}
+    try {
+        await m.react('🕒');
 
-handler.command = /^(ytmp3)$/i
+        let api = await fetch(`https://axeel.my.id/api/download/audio?url=${text}`);
+        let json = await api.json();
+        let { title, url, views, thumbnail, likes, description, author } = json.metadata;
 
-export default handler
+      const doc = {
+      audio: { url: json.downloads.url },
+      mimetype: 'audio/mpeg',
+      fileName: `${title}.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: url,
+          title: title,
+          sourceUrl: url,
+          thumbnail: thumbnail,
+        }
+      }
+    };
+    await conn.sendMessage(m.chat, doc, { quoted: m })
+        await m.react('✅');
+    } catch (error) {
+        console.error(error);
+        await m.react('❌');
+        conn.reply(m.chat, `☁️ Hubo un error al procesar tu solicitud. Inténtalo de nuevo más tarde.`, m);
+    }
+};
+
+handler.help = ['ytmp3 *<url>*'];
+handler.tags = ['dl'];
+handler.command = /^(ytmp3)$/i;
+
+export default handler;
