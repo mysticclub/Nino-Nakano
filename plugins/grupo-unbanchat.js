63e0e91b6b202edb8367e7baec58c8674f7b2d37
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 23:21:49 -0400

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,4 +1,4 @@
-let handler = async (m) => {
+/* let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
 
     m.reply('CHAT DESBANEADO')
@@ -10,4 +10,28 @@ handler.botAdmin = true
 handler.admin = true 
 handler.group = true
 
-export default handler
+export default handler */
+
+let handler = async (m, { isOwner }) => {
+    // Validar que el comando lo ejecute el dueño del bot
+    if (!isOwner) {
+        return m.reply('Este comando solo puede ser usado por el dueño del bot.');
+    }
+
+    // Validar que el chat exista en la base de datos
+    if (!global.db.data.chats[m.chat]) {
+        global.db.data.chats[m.chat] = {}; // Crear entrada si no existe
+    }
+
+    // Cambiar el estado de baneo
+    global.db.data.chats[m.chat].isBanned = false;
+    m.reply('CHAT DESBANEADO');
+};
+
+// Configuración del comando
+handler.help = ['unbanchat'];
+handler.tags = ['owner'];
+handler.command = ['unbanchat']; // Puede aceptar múltiples alias
+handler.rowner = true; // Requiere permisos de dueño del bot
+
+export default handler;
