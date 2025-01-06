a66d3f62fd519757db091bd8f5a2c754908b5e33
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 23:42:40 -0400

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,6 +1,6 @@
 var handler = async (m) => {
 
-global.db.data.chats[m.chat].isBanned = false
+global.db.data.chats[m.chat].isBanned = false;
 conn.reply(m.chat, '✅ *Este chat fue desbaneado con éxito*', m, fake, )
 
 }
