6868a052cae2eef1cfebe94ce6ac962a39f6e10e
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 23:41:07 -0400

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,12 +1,15 @@
+let handler = async (m) => {
 
-let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = true
-    m.reply(`✅ Chat Baneado exitosamente`)
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `✅ *Este chat fue baneado con éxito*`, m, fake, )
+
 }
 handler.help = ['banchat']
-handler.tags = ['owner']
-handler.command = ['banchat', 'chatoff'] 
+handler.tags = ['own']
+handler.command = /^banchat$/i
+
+handler.botAdmin = true
+handler.admin = true 
 handler.group = true
 
 export default handler
