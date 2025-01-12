734c09eed1a053f8ed1d90a37c42e06c75f49db9
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-12 16:30:30 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -17,35 +17,50 @@ https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-// *[ ❀ YTMP4 ]*
+// *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
 try {
-let api = await fetch(`https://delirius-apiofc.vercel.app/download/ytmp4?url=${text}`)
+let api = await fetch(`https://delirius-apiofc.vercel.app/download/ytmp3?url=${text}`)
 let json = await api.json()
 let { title, author, image:img, id, views, likes, comments, duration, download } = json.data
 let HS = `- *Titulo :* ${title}
 - *Autor :* ${author}
-- *Visitas :* ${views}
-- *Likes :* ${likes}
-- *Comentarios :* ${comments}
+- *Visitas :* ${Num(views)}
+- *Likes :* ${Num(likes)}
+- *Comentarios :* ${Num(comments)}
 
 *[ INFO ARCHIVO AUDIO ]*
 
 - *Tamaño :* ${download.size}
 - *Calidad :* ${download.quality}`
 await conn.sendFile(m.chat, img, 'HasumiBotFreeCodes.jpg', HS, m)
-await await conn.sendMessage(m.chat, { video: { url: download.url }, fileName: `${title}.mp4`, mimetype: 'video/mp4', caption: `` }, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: download.url }, mimetype: 'audio/mpeg' }, { quoted: m });
 } catch (error) {
 console.error(error)
 }}
 
-handler.command = /^(ytmp4)$/i
+handler.command = /^(ytmp3)$/i
 
 export default handler
+
+function Num(number) {
+if (number >= 1000 && number < 1000000) {
+return (number / 1000).toFixed(1) + 'k'
+} else if (number >= 1000000) {
+return (number / 1000000).toFixed(1) + 'M'
+} else if (number <= -1000 && number > -1000000) {
+return (number / 1000).toFixed(1) + 'k'
+} else if (number <= -1000000) {
+return (number / 1000000).toFixed(1) + 'M'
+} else {
+return number.toString()
+}}
+
+
 
 
 
