86f5d8e812fd47f691bec122197832818a92772c
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 18:26:50 -0400

    Actualizar game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -21,7 +21,7 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `🍪 *Cookies*` + ' que deseas aportar contra' + ` *YaemoriBot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `🤍 *corazones*` + ' que deseas aportar contra' + ` *Genesis-Ai*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 if (user.cookies >= count * 1) {
 user.cookies -= count * 1
 if (Aku > Kamu) {
