6f6a87e6220e96336a558c9cefca7706287a48f9
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-13 20:10:13 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,72 +1,4 @@
-//Los que no dejan creditos se la comen 🗣🗣
-
-/* 
-
-*❀ By JTxs*
-
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ YTMP3 ]*
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
-
-try {
-let api = await fetch(`https://delirius-apiofc.vercel.app/download/ytmp3?url=${text}`)
-let json = await api.json()
-let { title, author, image:img, id, views, likes, comments, duration, download } = json.data
-let HS = `- *Titulo :* ${title}
-- *Autor :* ${author}
-- *Visitas :* ${Num(views)}
-- *Likes :* ${Num(likes)}
-- *Comentarios :* ${Num(comments)}
-
-*[ INFO ARCHIVO AUDIO ]*
-
-- *Tamaño :* ${download.size}
-- *Calidad :* ${download.quality}`
-await conn.sendFile(m.chat, img, 'HasumiBotFreeCodes.jpg', HS, m)
-await conn.sendMessage(m.chat, { audio: { url: download.url }, mimetype: 'audio/mpeg' }, { quoted: m });
-} catch (error) {
-console.error(error)
-}}
-
-handler.help = ['ytmp3 *<url>*']
-handler.tags = ['dl']
-handler.command = /^(ytmp3)$/i
-
-export default handler
-
-function Num(number) {
-if (number >= 1000 && number < 1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number >= 1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else if (number <= -1000 && number > -1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number <= -1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else {
-return number.toString()
-}}
-
-
-
-
-
-/* import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
     if (!text) {
@@ -119,4 +51,4 @@ handler.help = ['ytmp3 *<url>*']
 handler.tags = ['dl']
 handler.command = ['ytmp3', 'fgmp3', 'yta'];
 
-export default handler; */
+export default handler;
