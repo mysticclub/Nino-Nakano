663bbfe06859748705bc6a590e3a3d5222ea47e6
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 17:13:54 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -34,7 +34,7 @@ conn.sendMessage(m.chat, {
         mimetype: "audio/mpeg",
         fileName: ytData.title,
         caption: "> descargue el documento para escuchar la música\n\n> *Presione el botón Descargar arriba.*",
-        jpegThumbnail: await conn.resize(mp3.ytData.thumbnail, 400, 400),
+        jpegThumbnail: await conn.resize(ytData.thumbnail, 400, 400),
       }, { quoted: m });
 
 
