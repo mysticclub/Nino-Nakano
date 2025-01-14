dd241aaf481b20e0131ff44f94e91aa954bc9e8e
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 18:29:49 -0400

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -15,6 +15,6 @@ await conn.reply(m.chat, txt, m, rcanal)
 
 handler.help = ['corazones']
 handler.tags = ['rpg']
-handler.command = ['wallet', 'cartera', 'cookies', 'bal', 'galletas']
+handler.command = ['wallet', 'cartera', 'corazones', 'bal', 'galletas']
 handler.register = true 
 export default handler
