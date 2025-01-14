582a88b4b387bdc6d9d12bfa9620e658fd8a87ae
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 16:56:32 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,3 +1,54 @@
+import yts from 'yt-search';
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Por favor, proporciona una URL de YouTube';
+
+  // Validar si es una URL válida de YouTube
+  const ytRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+  if (!ytRegex.test(text)) throw 'La URL proporcionada no es válida. Asegúrate de que sea un enlace de YouTube';
+
+  let src = await yts({ videoId: text.split('v=')[1] || text.split('/').pop() });
+  let yt = src.videos[0];
+
+  if (!yt) throw 'No se encontró información para este enlace. Por favor, verifica la URL';
+
+  // Enviar el mensaje con la miniatura y título
+  await conn.sendMessage(m.chat, { 
+    image: { url: yt.thumbnail }, 
+    caption: yt.title 
+  }, { quoted: m });
+
+  // Enviar el archivo de audio
+  return conn.sendMessage(m.chat, {
+    audio: {
+      url: `https://kepolu-ytdl.hf.space/yt/dl?url=${yt.url}&type=audio`
+    },
+    mimetype: 'audio/mpeg',
+    contextInfo: {
+      externalAdReply: {
+        title: yt.title,
+        body: 'PLAY AUDIO',
+        mediaType: 2,
+        mediaUrl: yt.url,
+        thumbnailUrl: yt.thumbnail,
+        sourceUrl: yt.url,
+        containsAutoReply: true,
+        renderLargerThumbnail: true,
+        showAdAttribution: false,
+      }
+    }
+  }, { quoted: m });
+};
+
+handler.help = ['playyt']
+handler.command = ['playyt']
+handler.tags = ['downloader']
+export default handler;
+
+
+
+
+
 
 /* import yts from 'yt-search';
 
