4b19335c59f8a1a88a3974794b7f2b2056d9b46a
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 16:47:33 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,4 +1,25 @@
-import fetch from 'node-fetch';
+// *[ ❀ YTMP4 ]*
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
+    
+try {
+let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
+let dl_url = api.data.dl
+
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['ytmp4']
+
+export default handler
+
+
+
+/* import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
   if (!text) {
@@ -52,4 +73,4 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['ytmp4 *<url>*']
 handler.tags = ['dl']
 handler.command = ['ytmp4'];
-export default handler;
+export default handler; */
