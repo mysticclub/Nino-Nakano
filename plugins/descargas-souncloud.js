66720c3835088dd6418f98977d374305a9e7122a
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-14 19:03:14 -0400

    Actualizar descargas-souncloud.js

--- plugins/descargas-souncloud.js
+++ plugins/descargas-souncloud.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `🚩 Ingrese el nombre de la cancion de *Soundcloud.*`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🤍 Ingrese el nombre de la cancion de *Soundcloud.*`, m, rcanal)
 
 await m.react('🕒');
 try {
@@ -21,7 +21,7 @@ let txt = `*\`- S O U N C L O U D - M U S I C -\`*\n\n`;
     txt += `*Título* : ${json[0].title}\n`;
     txt += `*Calidad* : ${quality}\n`;
     txt += `*Url* : ${url}\n\n`;
-    txt += `> 🚩 *${dev}*`
+    txt += `> *${dev}*`
 
 await conn.sendFile(m.chat, image, 'thumbnail.jpg', txt, m, null, rcanal);
 await conn.sendMessage(m.chat, { audio: audio, fileName: `${json[0].title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
@@ -32,7 +32,7 @@ await m.react('✖️');
 }}
 
 handler.help = ['soundcloud *<texto>*']
-handler.tags = ['downloader']
+handler.tags = ['dl']
 handler.command = ['soundcloud', 'sound']
 
 export default handler
