283cc4639782182d4c2576a6093408c7bea399a7
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 17:19:30 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,29 +1,33 @@
-import { igdl } from "ruhend-scraper";
+import axios from 'axios'
 
-let handler = async (m, { args, conn }) => { 
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     if (!args[0]) {
-        return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, fake);
+        await m.react('✖️') // Reacción de error por falta de enlace
+        return conn.reply(m.chat, `❀ Ingresa un link de Instagram`, m)
     }
-    
-    try {
-        await m.react('🕑');
-        
-        let res = await igdl(args[0]);
-        let data = res.data; 
-        
-        for (let media of data) {
-            await new Promise(resolve => setTimeout(resolve, 2000));
 
-            await m.react('✅');
-            await conn.sendFile(m.chat, media.url, 'instagram.mp4', dev, null, m); 
+    if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) {
+        await m.react('✖️') // Reacción de error por enlace inválido
+        return conn.reply(m.chat, `❀ Verifica que sea un link de Instagram`, m)
+    }
+
+    try {
+        await m.react('🕑')
+        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`)
+        for (let a of api.data.data) {
+            if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+                await conn.sendMessage(m.chat, { image: { url: a.url } }, { quoted: m })
+            } else {
+                await conn.sendMessage(m.chat, { video: { url: a.url } }, { quoted: m })
+            }
         }
-    } catch {
-        await m.react('❌');
+        await m.react('✅')
+    } catch (error) {
+        console.log(error)
+        await m.react('❌')
     }
 }
 
-handler.command = ['ig', 'igdl', 'instagram'];
-handler.tags = ['dl'];
-handler.help = ['ig *<link>*'];
+handler.command = /^(instagramdl|igdl|ig|instagram)$/i
 
-export default handler;
+export default handler
