7633b690bae8dac1dd6fb6d321ad122e76e4b8c5
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 17:13:16 -0400

    Actualizar descargas-ytmp4Doc.js

--- plugins/descargas-ytmp4Doc.js
+++ plugins/descargas-ytmp4Doc.js
@@ -4,7 +4,6 @@ let handler = async (m, { text, conn, args, usedPrefix, command }) => {
     if (!args[0]) {
         await m.react('✖️')
         return conn.reply(m.chat, '☁️ Ingresa un enlace de videy.', m, fake);
-      //  return m.reply("Ingresa un link de videy")
     }
 
     try {
@@ -24,30 +23,3 @@ let handler = async (m, { text, conn, args, usedPrefix, command }) => {
 handler.command = ['videydl']
 
 export default handler
-
-
-
-
-
-
-// BY JTXS
-// Creado : 21/11/24
- 
-/* import fetch from "node-fetch"
- 
-let handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply("ingresa un link de videy")
- 
-try {
-let api = await fetch(`https://api.agatz.xyz/api/videydl?url=${args[0]}`)
-let json = await api.json()
-let { data } = json
- 
-await conn.sendFile(m.chat, data, 'HasumiBotFreeCodes.mp4', null, m)
-} catch (error) {
-console.error(error)
-}}
- 
-handler.command = ['videydl']
- 
-export default handler */
