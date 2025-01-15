90d10d84031cfadec1729dafcf9a0d3f81d65858
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 18:05:53 -0400

    Actualizar descargas-cocofun.js

--- plugins/descargas-cocofun.js
+++ plugins/descargas-cocofun.js
@@ -31,32 +31,3 @@ handler.tags = ['dl'];
 handler.command = ['cocofundl']
 
 export default handler
-
-
-
-
-/* import fetch from "node-fetch"
- 
-let handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply("ingresa un link de cocofun")
- 
-try {
-let api = await fetch(`https://api.agatz.xyz/api/cocofundl?url=${args[0]}`)
-let json = await api.json()
-let { title, description, image, video, topic, caption, play, like, share, duration, thumbnail, watermark, no_watermark } = json.data
-let JT = `*Title :* ${title}
-*Descripcion :* ${description}
-*Visitas :* ${play}
-*Likes :* ${like}
-*Duracion :* ${duration}
-`
- 
-await conn.sendFile(m.chat, image, 'HasumiBotFreeCodes.jpg', JT, m)
-await conn.sendFile(m.chat, no_watermark, 'HasumiBotFreeCodes.mp4', JT, m)
-} catch (error) {
-console.error(error)
-}}
- 
-handler.command = ['cocofundl']
- 
-export default handler */
