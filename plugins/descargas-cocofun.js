3a7f78fb3e0551ba27a8829453fa71e00e558270
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 17:58:04 -0400

    Actualizar descargas-cocofun.js

--- plugins/descargas-cocofun.js
+++ plugins/descargas-cocofun.js
@@ -1,5 +1,39 @@
-
 import fetch from "node-fetch"
+
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+        await m.react('✖️')
+        return m.reply("ingresa un link de cocofun")
+    }
+    
+    try {
+        await m.react('🕒') 
+        let api = await fetch(`https://api.agatz.xyz/api/cocofundl?url=${args[0]}`)
+        let json = await api.json()
+        let { title, description, image, video, topic, caption, play, like, share, duration, thumbnail, watermark, no_watermark } = json.data
+        let txt = `*Title :* ${title}
+*Descripcion :* ${description}
+*Visitas :* ${play}
+*Likes :* ${like}
+*Duracion :* ${duration}
+`
+        await conn.sendFile(m.chat, image, 'image.jpg', txt, m)
+        await conn.sendFile(m.chat, no_watermark, 'video.mp4', txt, m)
+        await m.react('✅')
+    } catch (error) {
+        console.error(error)
+        await m.react('❌')
+    }
+}
+
+handler.command = ['cocofundl']
+
+export default handler
+
+
+
+
+/* import fetch from "node-fetch"
  
 let handler = async (m, { text, conn, args, usedPrefix, command }) => {
 if (!args[0]) return m.reply("ingresa un link de cocofun")
@@ -23,4 +57,4 @@ console.error(error)
  
 handler.command = ['cocofundl']
  
-export default handler
+export default handler */
