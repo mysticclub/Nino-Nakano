c6d087f4c11d93fbbd03dcff94379d60a0de5b44
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 17:24:00 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,21 +1,3 @@
-/* 
-
-*❀ By JTxs*
-
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ INSTAGRAM DL (imagen|video) ]*
 import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
@@ -25,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     }
 
     if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) {
-        await m.react('✖️') // Reacción de error por enlace inválido
+        await m.react('✖️')
         return conn.reply(m.chat, `❀ Verifica que sea un link de Instagram`, m)
     }
 
