566da336578a4fd1036e41c093a858b6d8f94661
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 17:35:56 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -14,14 +14,14 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     try {
         await m.react('🕑')
         let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`).JSON
-        let dl_url = api.data.title
+        let title = api.data.title
         for (let a of api.data.data) {
             if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
                 await conn.sendMessage(
                     m.chat, 
                     { 
                         image: { url: a.url }, 
-                        caption: '> *[ I G - D O W N L O A D ]*\n' 
+                        caption: '> *[ I G - D O W N L O A D ]*\n ${title}' 
                     }, 
                     { quoted: m }
                 )
