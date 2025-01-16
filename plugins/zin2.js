2c8af6d21a6905ac1706f7bc7d4a9687b337331c
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-15 23:56:06 -0400

    Actualizar zin2.js

--- plugins/zin2.js
+++ plugins/zin2.js
@@ -1,4 +1,4 @@
-const handler = async (m, { conn, args, command }) => {
+const handler = async (m, { conn, args }) => {
 
     if (args.length < 3) {
         conn.reply(m.chat, '𝘋𝘦𝘣𝘦𝘴 𝘱𝘳𝘰𝘱𝘰𝘳𝘤𝘪𝘰𝘯𝘢𝘳 𝘭𝘢 𝘳𝘦𝘨𝘪𝘰𝘯 (SR o EU), 𝘭𝘢 𝘩𝘰𝘳𝘢 (𝘏𝘏:𝘔𝘔) 𝘺 𝘦𝘭 𝘱𝘢𝘪́𝘴 (𝘉𝘖, 𝘗𝘌, 𝘊𝘓, 𝘈𝘙, 𝘊𝘖, 𝘔𝘟).', m);
@@ -19,36 +19,32 @@ const handler = async (m, { conn, args, command }) => {
         '🇵🇪': 'PE',
         '🇨🇱': 'CL',
         '🇦🇷': 'AR',
-        '🇨🇴': 'CO', // Colombia
-        '🇲🇽': 'MX'  // México
+        '🇨🇴': 'CO',
+        '🇲🇽': 'MX'
     };
 
     if (banderasToPais[paisBase]) {
         paisBase = banderasToPais[paisBase];
     }
 
-    // Verificar la región
-    const region = args[0].toUpperCase(); // SR o EU
+    const region = args[0].toUpperCase();
     if (region !== 'SR' && region !== 'EU') {
         conn.reply(m.chat, '𝘓𝘢 𝘳𝘦𝘨𝘪𝘰𝘯 𝘦𝘯 𝘳𝘦𝘤𝘪𝘣𝘰 𝘯𝘰 𝘦𝘴 𝘷𝘢𝘭𝘪𝘥𝘢. 𝘜𝘴𝘢 𝘚𝘙 𝘰 𝘌𝘜.', m);
         return;
     }
 
-    // Diferencias horarias para SR (Sudamérica) - Bolivia, Perú, Chile, Argentina
     const diferenciasHorariasSR = {
-        BO: 0, // Bolivia
-        PE: -1, // Perú 
-        CL: 1,  // Chile
-        AR: 1,  // Argentina
+        BO: 0,
+        PE: -1,
+        CL: 1,
+        AR: 1
     };
 
-    // Diferencias horarias para EU (Europa) - Colombia y México
     const diferenciasHorariasEU = {
-        CO: -1, // Colombia
-        MX: -2  // México
+        CO: -1,
+        MX: -2
     };
 
-    // Usar las diferencias horarias correctas según la región
     const diferenciasHorarias = region === 'SR' ? diferenciasHorariasSR : diferenciasHorariasEU;
 
     if (!(paisBase in diferenciasHorarias)) {
@@ -83,9 +79,8 @@ const handler = async (m, { conn, args, command }) => {
 
     const formatTime = (date) => date.toLocaleTimeString('es', { hour12: false, hour: '2-digit', minute: '2-digit' });
 
-    // Generar el mensaje
     const message = `
-*4 𝐕𝐄𝐑𝐒𝐔𝐒 4 ${region === 'SR' ? '' : '(EU)'}
+4 𝐕𝐄𝐑𝐒𝐔𝐒 4 ${region === 'SR' ? '' : '(EU)'}
 
 ${horasEnPais[0].map(({ pais, hora }) => {
         const bandera = {
@@ -93,8 +88,8 @@ ${horasEnPais[0].map(({ pais, hora }) => {
             PE: '🇵🇪',
             CL: '🇨🇱',
             AR: '🇦🇷',
-            CO: '🇨🇴', // Colombia
-            MX: '🇲🇽'  // México
+            CO: '🇨🇴',
+            MX: '🇲🇽'
         }[pais];
         return `${bandera} ${pais} : ${formatTime(hora)}`;
     }).join('\n')}
@@ -112,77 +107,11 @@ ${horasEnPais[0].map(({ pais, hora }) => {
 🥷🏻 ┇
 `.trim();
 
-    // Enviar solo una lista con los horarios y un solo botón
     await m.react('✅')
-    conn.sendMessage(m.chat, {
-        text: message, 
-        caption: "1234", 
-        footer: wm, 
-        buttons: [
-            {
-                buttonId: `.anotar_${m.sender}`, 
-                buttonText: { 
-                    displayText: 'Anotar' 
-                }
-            }
-        ],
-        viewOnce: true,
-        headerType: 1,
-    }, { quoted: m });
+    conn.sendMessage(m.chat, { text: message }, { quoted: m });
 };
 
-// Función para manejar el evento cuando se presiona el botón "Anotar"
-const anotador = async (m, { conn, command }) => {
-    const { exp, corazones, name, registered, regTime, age, level } = global.db.data.users[m.sender];
-    
-    // Los nombres de los jugadores
-    let players = ['👑', '🥷🏻', '🥷🏻', '🥷🏻'];
-
-    // Buscar un espacio vacío y colocar el nombre del usuario
-    for (let i = 0; i < players.length; i++) {
-        if (players[i] === '🥷🏻') {
-            players[i] = `${name}`;
-            break; // Salir del ciclo una vez que se encuentra un espacio vacío
-        }
-    }
-
-    // Volver a generar el mensaje con los nombres
-    const message = `
-*4 𝐕𝐄𝐑𝐒𝐔𝐒 4*
-
-${players.join(' ┇ ')}
-
-𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔
-
-${players.join(' ┇ ')}
-
-ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
-🥷🏻 ┇ 
-🥷🏻 ┇
-`.trim();
-
-    // Enviar de nuevo el mensaje con los nombres actualizados
-    conn.sendMessage(m.chat, {
-        text: message, 
-        caption: "1234", 
-        footer: wm, 
-        buttons: [
-            {
-                buttonId: `.anotar_${m.sender}`, 
-                buttonText: { 
-                    displayText: 'Anotar' 
-                }
-            }
-        ],
-        viewOnce: true,
-        headerType: 1,
-    }, { quoted: m });
-};
-
-// Vincular el comando
 handler.help = ['4vs4']
 handler.tags = ['freefire']
 handler.command = /^(4vs4|vs4)$/i;
-handler.anotador = anotador;
-
 export default handler;
