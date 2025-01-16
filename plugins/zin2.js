eb5369aa20edf043e900c468a54bc27bf39896fd
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-16 00:19:32 -0400

    Actualizar zin2.js

--- plugins/zin2.js
+++ plugins/zin2.js
@@ -1,4 +1,4 @@
-const handler = async (m, { conn, args }) => {
+/* const handler = async (m, { conn, args }) => {
 
     if (args.length < 3) {
         conn.reply(m.chat, '𝘋𝘦𝘣𝘦𝘴 𝘱𝘳𝘰𝘱𝘰𝘳𝘤𝘪𝘰𝘯𝘢𝘳 𝘭𝘢 𝘳𝘦𝘨𝘪𝘰𝘯 (SR o EU), 𝘭𝘢 𝘩𝘰𝘳𝘢 (𝘏𝘏:𝘔𝘔) 𝘺 𝘦𝘭 𝘱𝘢𝘪́𝘴 (𝘉𝘖, 𝘗𝘌, 𝘊𝘓, 𝘈𝘙, 𝘊𝘖, 𝘔𝘟).', m);
@@ -114,4 +114,4 @@ ${horasEnPais[0].map(({ pais, hora }) => {
 handler.help = ['4vs4']
 handler.tags = ['freefire']
 handler.command = /^(4vs4|vs4)$/i;
-export default handler;
+export default handler; */
