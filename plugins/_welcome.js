69504644e8a63ca97e2e633cedb65c3f75d01346
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-17 11:49:50 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -19,7 +19,11 @@ await conn.sendAi(m.chat, packname, dev, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `❀ *se salio* del grupo  *${groupMetadata.subject}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    ʕ⁠´⁠•⁠ ⁠ᴥ⁠•̥⁠`⁠ʔ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
+    let bye = `❀ *Se salió* del grupo *${groupMetadata.subject}*\n` +
+    `    ✰ @${m.messageStubParameters[0].split('@')[0]}\n\n` +
+    `    ʕ⁠´⁠•⁠ ⁠ᴥ⁠•̥⁠`⁠ʔ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n` +
+    `> ✐ No olvides usar *#help* si necesitas algo.\n` +
+    `> 🜸 Próximamente...`;
 await conn.sendAi(m.chat, packname, dev, bye, img, img, canal, estilo)
   }
   
