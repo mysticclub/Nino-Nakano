60a839b9ca6e2deb22707103b38d6954d096074b
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-17 10:25:40 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -189,12 +189,13 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command }) => {
         if (plugin.tags && plugin.tags.includes(tag))
           if (plugin.help) groups[tag].push(plugin)
     }
-    
-    let before = conn.menu?.before || defaultMenu.before
-    let header = conn.menu?.header || defaultMenu.header
-    let body = conn.menu?.body || defaultMenu.body
-    let footer = conn.menu?.footer || defaultMenu.footer
-    let after = conn.menu?.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
     let _text = [
       before,
       ...Object.keys(emojis).map(tag => {
