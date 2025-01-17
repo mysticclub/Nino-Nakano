672159da221888a78f825739448a8367974af648
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-17 11:58:37 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
+  const groupName = (await conn.groupMetadata(m.chat)).subject;
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/Tdxwk.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
@@ -13,18 +14,18 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let userName = user ? user.name : await conn.getName(who);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Se unio* al grupo *${groupMetadata.subject}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ no olvides usar *#help* si necesitas algo.\n > 🜸 ¡Disfruta de tu tiempo con nosotros!`;
+    let bienvenida = `❀ *Se unio* al grupo *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ no olvides usar *#help* si necesitas algo.\n > 🜸 ¡Disfruta de tu tiempo con nosotros!`;
     
 await conn.sendAi(m.chat, packname, dev, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `❀ *se salio* del grupo  *${groupMetadata.subject}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
+    let bye = `❀ *se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
 
 await conn.sendAi(m.chat, packname, dev, bye, img, img, canal, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `❀ *se salio* del grupo  *${groupMetadata.subject}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
+    let kick = `❀ *se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
 await conn.sendAi(m.chat, packname, dev, kick, img, img, canal, estilo)
 }}
