1662d6de0dce8199605168abbeffc5a155b2547d
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-16 21:36:09 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -270,20 +270,45 @@ handler.exp = 3
 
 export default handler
 
-function ucapan() {
-  const hour = new Date().getHours();
-  if (hour >= 5 && hour < 12) {
-    return "¡Buenos días!";
-  } else if (hour >= 12 && hour < 18) {
-    return "¡Buenas tardes!";
-  } else {
-    return "¡Buenas noches!";
-  }
-};
+
+//----------- FUNCIÓN -------
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000); // Calcula las horas
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60; // Calcula los minutos
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60; // Calcula los segundos
-  return [h, 'H', m, 'M', s, 'S'].map(v => v.toString().padStart(2, '0')).join(' '); // Formatea el tiempo
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, ' H ', m, ' M ', s, ' S '].map(v => v.toString().padStart(2, 0)).join('')
+}
+function clockStringP(ms) {
+  let ye = isNaN(ms) ? '--' : Math.floor(ms / 31104000000) % 10
+  let mo = isNaN(ms) ? '--' : Math.floor(ms / 2592000000) % 12
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) % 30
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [ye, ' *Years 🗓️*\n', mo, ' *Month 🌙*\n', d, ' *Days ☀️*\n', h, ' *Hours 🕐*\n', m, ' *Minute ⏰*\n', s, ' *Second ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
+}
+function ucapan() {
+    const time = moment.tz('America/Buenos_Aires').format('HH')
+    let res = "Buenas Noches🌙"
+    if (time >= 5) {
+        res = "Buena Madrugada🌄"
+    }
+    if (time > 10) {
+        res = "Buenos días☀️"
+    }
+    if (time >= 12) {
+        res = "Buenas Tardes🌅"
+    }
+    if (time >= 19) {
+        res = "Buenas Noches🌙"
+    }
+    return res
 }
