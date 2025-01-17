61ab9d4db92c9064f773fc46a9a62c402f1a3c74
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-17 12:01:49 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  const groupName = (await conn.groupMetadata(m.chat)).subject;
+  let groupName = (await conn.groupMetadata(m.chat)).subject;
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/Tdxwk.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
@@ -14,7 +14,7 @@ export async function before(m, {conn, participants}) {
   let userName = user ? user.name : await conn.getName(who);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Se unio* al grupo *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ no olvides usar *#help* si necesitas algo.\n > 🜸 ¡Disfruta de tu tiempo con nosotros!`;
+    let bienvenida = `❀ *Se unio* al grupo *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ no olvides usar *#help* si necesitas algo.\n> 🜸 ¡Disfruta de tu tiempo con nosotros!`;
     
 await conn.sendAi(m.chat, packname, dev, bienvenida, img, img, canal, estilo)
   }
