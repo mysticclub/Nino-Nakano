9980ee129d2fd0825302a9fbf59a17d89382a787
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-16 21:29:47 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -269,3 +269,14 @@ handler.register = true
 handler.exp = 3
 
 export default handler
+
+function ucapan() {
+  const hour = new Date().getHours();
+  if (hour >= 5 && hour < 12) {
+    return "¡Buenos días!";
+  } else if (hour >= 12 && hour < 18) {
+    return "¡Buenas tardes!";
+  } else {
+    return "¡Buenas noches!";
+  }
+}
