bbf9af24ac4f18b6a7a6a4bc05c5bf688144d051
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-17 12:03:19 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -14,18 +14,18 @@ export async function before(m, {conn, participants}) {
   let userName = user ? user.name : await conn.getName(who);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Se unio* al grupo *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ no olvides usar *#help* si necesitas algo.\n> 🜸 ¡Disfruta de tu tiempo con nosotros!`;
+    let bienvenida = `❀ *Se unio* al grupo *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n        •(=^●ω●^=)• ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 ¡Disfruta de tu tiempo con nosotros!`;
     
 await conn.sendAi(m.chat, packname, dev, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `❀ *se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
+    let bye = `❀ *Se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
 
 await conn.sendAi(m.chat, packname, dev, bye, img, img, canal, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `❀ *se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
+    let kick = `❀ *Se salio* del grupo  *${groupName}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    •(=^●ω●^=)• ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Proximamente...`;
 await conn.sendAi(m.chat, packname, dev, kick, img, img, canal, estilo)
 }}
