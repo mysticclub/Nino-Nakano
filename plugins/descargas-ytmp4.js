a9b13ae46ecf0a10f1cf73e99d973585c4592389
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-18 12:39:09 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -23,6 +23,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
     
 try {
+await m.react('🕒');
 let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`)
 let json = await api.json()
 let { title, author, authorUrl, lengthSeconds, views, uploadDate, thumbnail, description, duration, downloadUrl, quality } = json
@@ -30,8 +31,10 @@ let HS = `*Titulo :* ${title}
 *Duracion :* ${duration}
 *Calidad :* ${quality}p`
 await conn.sendMessage(m.chat, { video: { url: downloadUrl }, caption: HS }, { quoted: m })
+await m.react('✅');
 } catch (error) {
 console.error(error)
+await m.react('✖️');
 }}
 
 handler.command = ['ytmp4']
