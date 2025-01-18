0f167c1f387ea4632bbac32f710ee97d3c59918c
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-18 14:16:37 -0400

    Actualizar start-mysn.js

--- plugins/start-mysn.js
+++ plugins/start-mysn.js
@@ -2,7 +2,7 @@ import db from '../lib/database.js'
 import { createHash } from 'crypto';
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6);
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', `乂 N U M E R O - D E - S E R I E 乂`, 'status@broadcast')
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', `N U M E R O - S E R I A L`, 'status@broadcast')
 }
 handler.help = ['mysn']
 handler.tags = ['start']
