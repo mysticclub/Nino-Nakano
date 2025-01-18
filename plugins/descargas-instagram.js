60589e753eff401a031d73f100d32dcc4e6869b4
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-18 18:20:25 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,57 +1,49 @@
-import axios from 'axios';
+import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     if (!args[0]) {
-        await m.react('✖️');
-        return conn.reply(m.chat, `☁️ Ingresa un link de Instagram`, m, fake);
+        await m.react('✖️')
+        return conn.reply(m.chat, `☁️ Ingresa un link de Instagram`, m, fake)
     }
 
     if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) {
-        await m.react('✖️');
-        return conn.reply(m.chat, `☁️ Verifica que sea un link de Instagram`, m);
+        await m.react('✖️')
+        return conn.reply(m.chat, `☁️ Verifica que sea un link de Instagram`, m)
     }
 
     try {
-        await m.react('🕑');
-        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`);
-        
-        // Set para almacenar las URLs ya procesadas
-        let processedUrls = new Set();
-
+        await m.react('🕑')
+        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`)
         for (let a of api.data.data) {
-            if (!processedUrls.has(a.url)) { // Verifica si la URL ya fue procesada
-                processedUrls.add(a.url); // Agrega la URL al Set
-
-                if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
-                    await conn.sendMessage(
-                        m.chat,
-                        { 
-                            image: { url: a.url }, 
-                            caption: '> *[ I G - D O W N L O A D ]*\n' 
-                        },
-                        { quoted: m }
-                    );
-                } else {
-                    await conn.sendMessage(
-                        m.chat,
-                        { 
-                            video: { url: a.url }, 
-                            caption: '> *[ I G - D O W N L O A D ]*\n' 
-                        },
-                        { quoted: m }
-                    );
-                }
+            if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+                await conn.sendMessage(
+                    m.chat, 
+                    { 
+                        image: { url: a.url }, 
+                        caption: '> *[ I G - D O W N L O A D ]*\n' 
+                    }, 
+                    { quoted: m }
+                )
+            } else {
+                await conn.sendMessage(
+                    m.chat, 
+                    { 
+                        video: { url: a.url }, 
+                        caption: '> *[ I G - D O W N L O A D ]*\n' 
+                    }, 
+                    { quoted: m }
+                )
             }
         }
-        await m.react('✅'); 
+        await m.react('✅') 
     } catch (error) {
-        console.log(error);
-        await m.react('❌');
+        console.log(error)
+        await m.react('❌')
     }
-};
+}
 
 handler.help = ['ig *<link>*'];
 handler.tags = ['dl'];
-handler.command = /^(ig|igdl|instagram)$/i;
+handler.command = /^(ig|igdl|instagram)$/i
 
-export default handler;
+export default handler
