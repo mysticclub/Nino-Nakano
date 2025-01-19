8356f7267dae942fbe6ed9030a0d64b3a1efbe24
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-19 13:25:11 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,22 +1,30 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, '❀ Ingresa un link de youtube', m)
+    if (!text) return conn.reply(m.chat, '❀ Ingresa un link de youtube', m);
     
     try {
         await m.react('🕒');
-        let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`)
+        let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`);
         let json = await api.json();
         let { title, author, authorUrl, lengthSeconds, views, uploadDate, thumbnail, description, duration, downloadUrl, quality } = json;
         
         let HS = `*Titulo :* ${title}\nDuración : ${duration}\nCalidad : ${quality}p`;
-        
+
         let durationInSeconds = parseInt(duration.split(':')[0]) * 60 + parseInt(duration.split(':')[1]);
-        
-        if (durationInSeconds >= 3600) {
-            await conn.sendMessage(m.chat, { document: { url: downloadUrl }, mimetype: 'video/mp4', fileName: `${title}.mp4`, caption: HS }, { quoted: m });
+
+        if (durationInSeconds >= 2400) {
+            await conn.sendMessage(m.chat, { 
+                document: { url: downloadUrl }, 
+                mimetype: 'video/mp4', 
+                fileName: `${title}.mp4`, 
+                caption: HS 
+            }, { quoted: m });
         } else {
-            await conn.sendMessage(m.chat, { video: { url: downloadUrl }, caption: HS }, { quoted: m });
+            await conn.sendMessage(m.chat, { 
+                video: { url: downloadUrl }, 
+                caption: HS 
+            }, { quoted: m });
         }
 
         await m.react('✅');
@@ -24,9 +32,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         console.error(error);
         await m.react('✖');
     }
-}
+};
 
-handler.command = ['ytmp4']
+handler.command = ['ytmp4'];
 
 export default handler;
 
