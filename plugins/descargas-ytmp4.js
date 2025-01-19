171606f2f673c377b156bf7aa164a98597470610
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-19 14:12:30 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,28 +1,49 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
-    
-try {
-await m.react('🕒');
-let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`)
-let json = await api.json()
-let { title, author, authorUrl, lengthSeconds, views, uploadDate, thumbnail, description, duration, downloadUrl, quality } = json
-let HS = `*Titulo :* ${title}
-*Duracion :* ${duration}
-*Calidad :* ${quality}p`
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, caption: HS }, { quoted: m })
-await m.react('✅');
-} catch (error) {
-console.error(error)
-await m.react('✖️');
-}}
+    if (!text) return conn.reply(m.chat, '❀ Ingresa un link de youtube', m);
+
+    try {
+        await m.react('🕒');
+        let api = await fetch(`https://apidl.asepharyana.cloud/api/downloader/ytmp4?url=${text}&quality=360`);
+        let json = await api.json();
+        let { title, author, authorUrl, lengthSeconds, views, uploadDate, thumbnail, description, duration, downloadUrl, quality } = json;
+        
+        let HS = `*Titulo :* ${title}\nDuración : ${duration}\nCalidad : ${quality}p`;
+
+        let durationInSeconds = 0;
+        if (duration.includes("min")) {
+            let minutes = parseFloat(duration.replace(" min", ""));
+            durationInSeconds = Math.round(minutes * 60); 
+        }
+
+        if (durationInSeconds >= 2400) {
+            await conn.sendMessage(m.chat, { 
+                document: { url: downloadUrl }, 
+                mimetype: 'video/mp4', 
+                fileName: `${title}.mp4`, 
+                caption: HS 
+            }, { quoted: m });
+        } else {
+            await conn.sendMessage(m.chat, { 
+                video: { url: downloadUrl }, 
+                caption: HS 
+            }, { quoted: m });
+        }
+
+        await m.react('✅');
+    } catch (error) {
+        console.error(error);
+        await m.react('✖');
+    }
+};
 
 handler.help = ['ytmp4 *<url>*'];
 handler.tags = ['dl'];
-handler.command = ['ytmp4']
+handler.command = ['ytmp4'];
 
-export default handler
+export default handler;
+
 
 
 
@@ -46,6 +67,8 @@ console.error(error)
 await m.react('✖️');
 }}
 
+handler.help = ['ytmp4 *<url>*'];
+handler.tags = ['dl'];
 handler.command = ['ytmp4']
 
 export default handler */
