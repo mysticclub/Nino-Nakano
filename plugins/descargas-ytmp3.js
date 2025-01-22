139114c38a72dbf04ac4b4286662cdc09f3e77c2
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-22 00:18:25 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,3 +1,52 @@
+import axios from 'axios';
+
+const handler = async (m, { text, conn }) => {
+    if (!text) return m.reply('Proporcióname el enlace de YouTube para que pueda ayudarte. 🎵');
+
+    try {
+       await m.react('🕓');
+
+        const response = await axios.get(`https://ytdl.axeel.my.id/api/download/audio/?url=${text}`);
+
+        if (!response.data || !response.data.metadata) {
+            return m.reply('No se pudo obtener los datos del enlace de YouTube. Asegúrate de que el enlace sea correcto. 😕');
+        }
+
+        const { metadata, downloads } = response.data;
+
+        const audioUrl = downloads.url;
+        const thumbnailUrl = metadata.thumbnail.url;
+
+        await conn.sendMessage(m.chat, {
+            image: {
+                url: thumbnailUrl
+            },
+            caption: `🎶 *Título*: ${metadata.title}\n⏳ *Duración*: ${metadata.duration}s\n👀 *Vistas*: ${metadata.views}`,
+        }, { quoted: m });
+
+        await conn.sendMessage(m.chat, {
+            audio: {
+                url: audioUrl
+            },
+            caption: `*• Título*: ${metadata.title}`,
+        }, { quoted: m });
+        await m.react('✅');
+
+    } catch (error) {
+        await m.react('✖️');
+    }
+};
+
+handler.help = ['ytmp3 *<url>*'];
+handler.tags = ['dl'];
+handler.command = ['ytmp3'];
+export default handler;
+
+
+
+
+
+
 /* import fetch from "node-fetch";
 import yts from "yt-search";
 
