0eb6da028b518be62681f896c0b6bb34318104da
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-22 00:14:15 -0400

    Actualizar descargas-ytmp4Doc.js

--- plugins/descargas-ytmp4Doc.js
+++ plugins/descargas-ytmp4Doc.js
@@ -1,4 +1,4 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+/* import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 import yts from 'yt-search'
 import ytdl from 'ytdl-core'
@@ -372,4 +372,4 @@ if (data.status === 'ok') {
   } else {
     throw new Error("No se pudo obtener la descarga desde 9Convert");
   }
-}
+} */
