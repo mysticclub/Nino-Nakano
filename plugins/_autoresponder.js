13855ae19dd82378c5681e1970ca270b6d7c31a3
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-21 21:40:34 -0400

    Actualizar _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { franc } from 'franc-min';
-import { translate } from '@vitalets/google-translate-api'; 
+import translateModule from '@vitalets/google-translate-api';
 
 let handler = m => m;
 
