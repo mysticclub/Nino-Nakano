09ca7d2ab69bdc836928d88d53a569d1860a71a2
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-23 13:42:54 -0400

    Actualizar main-menuff.js

--- plugins/main-menuff.js
+++ plugins/main-menuff.js
@@ -1,4 +1,4 @@
-import { promises } from 'fs'
+/* import { promises } from 'fs'
 import { join } from 'path'
 import { xpRange } from '../lib/levelling.js'
 import moment from 'moment-timezone'
@@ -246,4 +246,4 @@ function ucapan() {
     res = "Noche 🌙"
   }
   return res
-}
+} */
