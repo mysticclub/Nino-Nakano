4c7db0ed1a3726db537147b7f8ef5d73c25d7d64
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-24 12:54:47 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,4 +1,46 @@
 import axios from 'axios';
+
+const handler = async (m, { conn, text }) => {
+    if (!text) {
+        return m.reply('¿Nombre?');
+    }
+    try {
+        // Petición a la API
+        const { data } = await axios.post(`https://github-roaster.programordie.workers.dev/${text}`);
+        const roastMessageInEnglish = data.roast || 'Vaya, parece que me quedé sin palabras para roastarlo.';
+        
+        // Traducción al español
+        const translation = await axios.post('https://translate.googleapis.com/translate_a/single', null, {
+            params: {
+                client: 'gtx',
+                sl: 'en',
+                tl: 'es',
+                dt: 't',
+                q: roastMessageInEnglish
+            }
+        });
+        const roastMessageInSpanish = translation.data[0][0][0];
+
+        // Envío del mensaje traducido
+        await conn.sendMessage(m.chat, {
+            text: `🔥 *¡Hora de roast!* 🔥\n\n${roastMessageInSpanish}`
+        }, { quoted: m });
+
+    } catch (error) {
+        console.error('Error:', error);
+        await m.reply('Ups, ocurrió un error. Intenta nuevamente, tal vez el código esté teniendo problemas.');
+    }
+};
+
+handler.help = ['roast nombre'];
+handler.tags = ['fun'];
+handler.command = /^(roast|roastme)$/i;
+
+export default handler;
+
+
+
+/* import axios from 'axios';
 const handler = async (m, {
     conn,
     text
@@ -25,4 +67,4 @@ const handler = async (m, {
 handler.help = ['roast nombre'];
 handler.tags = ['fun'];
 handler.command = /^(roast|roastme)$/i;
-export default handler;
+export default handler; */
