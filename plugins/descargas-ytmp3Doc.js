362b0c16b6e4e4db6f82bad9d0e56bcf04152b89
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-23 22:42:24 -0400

    Actualizar descargas-ytmp3Doc.js

---
+++ plugins/descargas-ytmp3Doc.js
@@ -0,0 +1,30 @@
+// Código Creado Por darkcore Wa.me/51968382008
+import fetch from 'node-fetch';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+
+if (!text) return conn.reply(m.chat, `* Ingresa un texto para generar tu imagen a tu gusto*`,m,rcanal);
+m.react('🕒');
+await conn.reply(m.chat,'*🧑‍💻 Espere, Estamos Trabajando en su imagen*',m,rcanal);
+try {
+
+const response = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
+
+if (!response.ok) throw new Error('Network response was not ok');
+
+const buffer = await response.buffer();
+
+m.react('✔️');
+
+await conn.sendFile(m.chat, buffer, 'imagen.jpg',listo,m,rcanal,fake);
+} catch (error) {
+console.error(error);
+throw `*🚨 Lo sentimos, ha ocurrido un error 😔*`;
+}
+}
+
+handler.tags = ['tools'];
+handler.help = ['genearimg'];
+handler.command = ['genearimg', 'imgg'];
+handler.register = true;
+
+export default handler;
