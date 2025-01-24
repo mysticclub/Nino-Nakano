c9f0bd208d200b27ed957e369dca64dd1c5edd2c
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-24 12:32:14 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -37,6 +37,6 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['ytmp4 *<url>*'];
 handler.command = ['ytmp4'];
-handler.tags = ['downloader'];
+handler.tags = ['dl'];
 
 export default handler;
