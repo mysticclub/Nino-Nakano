3adee3e2e995ea26321bb2da4ec145f38e216bd2
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-25 19:35:19 -0400

    Actualizar internet-google.js

--- plugins/internet-google.js
+++ plugins/internet-google.js
@@ -6,6 +6,51 @@ let handler = async (m, { text }) => {
     return;
   }
 
+  const apiKey = 'xenzpedo';
+  const apiUrl = `https://api.botcahx.eu.org/api/search/google?text1=${encodeURIComponent(text)}&apikey=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+
+    if (!result.status) {
+      m.reply(`*Error al realizar la búsqueda:* ${result.message}`);
+      return;
+    }
+
+    let replyMessage = '*Resultados de búsqueda:*\n\n';
+    result.data.slice(0, 1).forEach((item, index) => {
+      replyMessage += `${index + 1}. ${item.title}\n`;
+      replyMessage += `> *${item.description}*\n\n`;
+      replyMessage += `   URL: ${item.url}\n\n`;
+    });
+
+    m.react('✅');
+    m.reply(replyMessage);
+  } catch (error) {
+    console.error('Error al realizar la solicitud a la API:', error);
+    m.reply('Ocurrió un error al obtener los resultados.');
+  }
+};
+
+handler.help = ['google *<texto>*'];
+handler.tags = ['internet'];
+handler.command = ['google'];
+
+export default handler;
+
+
+
+
+
+/* import fetch from 'node-fetch';
+
+let handler = async (m, { text }) => {
+  if (!text) {
+    m.reply('*Proporciona una consulta de búsqueda*');
+    return;
+  }
+
   const apiUrl = `https://delirius-apiofc.vercel.app/search/googlesearch?query=${encodeURIComponent(text)}`;
 
   try {
@@ -37,4 +82,4 @@ handler.help = ['google *<texto>*']
 handler.tags = ['internet']
 handler.command = ['google'];
 
-export default handler;
+export default handler; */
