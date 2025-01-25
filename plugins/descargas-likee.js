dc5c1c168d66b3ec656fe9fb0aa2f403183714a9
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-25 19:20:58 -0400

    Actualizar descargas-likee.js

--- plugins/descargas-likee.js
+++ plugins/descargas-likee.js
@@ -8,7 +8,7 @@ try {
 let app = await fetch(`https://apis-starlights-team.koyeb.app/starlight/like-downloader?url=${text}`, { headers: { 'Content-Type': 'application/json' }})
 let json = await app.json()
 let video = json.links['no watermark']
-await conn.sendFile(m.chat, video, 'samu.mp4', `${json.caption}`, m, null, rcanal)
+await conn.sendFile(m.chat, video, 'samu.mp4', `${json.caption}`, m)
 await m.react('✅')
 } catch {
 await m.react('✖️')
