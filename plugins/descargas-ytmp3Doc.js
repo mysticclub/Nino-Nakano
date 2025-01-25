4d014a6d3a592d5d5e0ec391043c997528aeefb2
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-25 13:16:40 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,67 +1,163 @@
 import axios from 'axios';
+import cheerio from 'cheerio';
+import path from 'path';
+import fetch from "node-fetch"
 
-const handler = async (m, { conn, text }) => {
-    if (!text) {
-        return m.reply('¿Nombre?');
+let handler = async (m, { conn, args, text }) => {
+        if (!args[0]) throw m.m.reply('✧ Ingresa un link de:\n- YouTube\n- instagram\n- tiktok\n- facebook\n- twitter ')
+
+await conn.sendMessage(m?.chat, {react: {text: `🌟`, key: m?.key}});
+class Fuck extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "Fuck";
     }
-    try {
-        const { data } = await axios.post(`https://github-roaster.programordie.workers.dev/${text}`);
-        const roastMessageInEnglish = data.roast || 'Vaya, parece que me quedé sin palabras para roastarlo.';
-        
-        const translation = await axios.post('https://translate.googleapis.com/translate_a/single', null, {
-            params: {
-                client: 'gtx',
-                sl: 'en',
-                tl: 'es',
-                dt: 't',
-                q: roastMessageInEnglish
-            }
-        });
-        const roastMessageInSpanish = translation.data[0][0][0];
+}
 
-        await conn.sendMessage(m.chat, {
-            text: `🔥 *¡Hora de roast!* 🔥\n\n${roastMessageInSpanish}`
-        }, { quoted: m });
+class API {
+    constructor(search, prefix) {
+        this.api = {
+            search: search,
+            prefix: prefix
+        };
+    }
+
+    headers(custom = {}) {
+        return {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'authority': 'retatube.com',
+            'accept': '*/*',
+            'accept-language': 'id-MM,id;q=0.9',
+            'hx-current-url': 'https://retatube.com/',
+            'hx-request': 'true',
+            'hx-target': 'aio-parse-result',
+            'hx-trigger': 'search-btn',
+            'origin': 'https://retatube.com',
+            'referer': 'https://retatube.com/',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'user-agent': 'Postify/1.0.0',
+            ...custom
+        };
+    }
+
+    handleError(error, context) {
+        const errors = error.response ? JSON.stringify(error.response.data || error.message) : error.message;
+        console.error(`[${context}] Error:`, errors);
+        throw new Fuck(errors);
+    }
+
+    getEndpoint(name) {
+        return this.api[name];
+    }
+}
+
+class RetaTube extends API {
+    constructor() {
+        super('https://retatube.com/api/v1/aio/search', 'https://retatube.com/api/v1/aio/index?s=retatube.com');
+    }
+
+    async getPrefix() {
+        try {
+            const response = await axios.get(this.getEndpoint('prefix'));
+            return this.scrapePrefix(response.data); 
+        } catch (error) {
+            this.handleError(error, 'GetPrefix');
+        }
+    }
+
+    scrapePrefix(htmlContent) {
+        const $ = cheerio.load(htmlContent);
+        const prefix = $('#aio-search-box input[name="prefix"]').val();
+        return prefix;
+    }
+
+    async fetch(videoId) {
+        try {
+            const prefix = await this.getPrefix();
+            const response = await axios.post(this.getEndpoint('search'), `prefix=${encodeURIComponent(prefix)}&vid=${encodeURIComponent(videoId)}`, { headers: this.headers() });
+            return this.parseHtml(response.data);
+        } catch (error) {
+            this.handleError(error, 'Fetch');
+        }
+    }
+
+    parseHtml(htmlContent) {
+        const $ = cheerio.load(htmlContent);
+        const result = {
+            title: '',
+            description: '',
+            videoLinks: [],
+            audioLinks: []
+        };
+
+        $('.col').each((_, element) => {
+            const titles = $(element).find('#text-786685718 strong').first();
+            result.title = titles.text().replace('Title：', '').trim() || result.title;
+
+            const description = $(element).find('.description').text();
+            result.description = description.trim() || '';
+
+            $(element).find('a.button.primary').each((_, linkElement) => {
+                const linkUrl = $(linkElement).attr('href');
+                const quality = $(linkElement).find('span').text().toLowerCase();
+
+                if (linkUrl !== 'javascript:void(0);') {
+                    if (quality.includes('audio')) {
+                        result.audioLinks.push({
+                            quality: quality,
+                            url: linkUrl
+                        });
+                    } else {
+                        result.videoLinks.push({
+                            quality: quality,
+                            url: linkUrl
+                        });
+                    }
+                }
+            });
+        });
+
+        return result;
+    }
+
+    async scrape(links) {
+        try {
+            return await this.fetch(links);
+        } catch (error) {
+            console.error(`${error.message}`);
+            throw error;
+        }
+    }
+}
+
+    const retatube = new RetaTube();
+    try {
+        const result = await retatube.scrape(text);
+        let videoMessage = `*Titulo*: ${result.title}\n*Descripción*: ${result.description}\n\n*Video*:`;
+        let audioMessage = `*Audio*:`;
+
+        // Mengirimkan video
+        if (result.videoLinks.length > 0) {
+            const video = result.videoLinks[0]; // Mengambil video dengan kualitas terbaik
+            await conn.sendMessage(m.chat, { video: { url: video.url }, caption: videoMessage }, { quoted: m });
+        } else {
+            await m.reply("Error.");
+        }
+
+
 
     } catch (error) {
-        console.error('Error:', error);
-        await m.reply('Ups, ocurrió un error. Intenta nuevamente, tal vez el código esté teniendo problemas.');
+        await m.reply(`Error: ${error.message}`);
     }
-};
+}
 
-handler.help = ['roast nombre'];
-handler.tags = ['fun'];
-handler.command = /^(roast|roastme)$/i;
+handler.help = ['aio'].map(v => v + ' *<link>*')
+handler.tags = ['downloader']
+handler.command = /^(aio)$/i
 
-export default handler;
+handler.limit = true
+handler.register = true
 
-
-
-/* import axios from 'axios';
-const handler = async (m, {
-    conn,
-    text
-}) => {
-    if (!text) {
-        return m.reply('¿Nombre?');
-    }
-    try {
-        const {
-            data
-        } = await axios.post(`https://github-roaster.programordie.workers.dev/${text}`);
-        const roastMessage = data.roast || 'Vaya, parece que me quedé sin palabras para roastarlo.';
-        await conn.sendMessage(m.chat, {
-            text: `🔥 *¡Hora de roast!* 🔥\n\n${roastMessage}`
-        }, {
-            quoted: m
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        await m.reply('Ups, ocurrió un error. Intenta nuevamente, tal vez el código esté teniendo problemas.');
-    }
-};
-
-handler.help = ['roast nombre'];
-handler.tags = ['fun'];
-handler.command = /^(roast|roastme)$/i;
-export default handler; */
+export default handler
