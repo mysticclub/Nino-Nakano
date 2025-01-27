9c45e2e1a2f585aa27222cfc139800d1c1529145
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-26 20:06:48 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -6,6 +6,69 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
         return conn.reply(m.chat, `☁️ Ingresa un link de Instagram`, m, fake);
     }
 
+    // Expresión regular actualizada para aceptar enlaces de publicaciones, reels y historias
+    if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel|s)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Verifica que sea un link de Instagram válido (publicación, reel o historia)`, m);
+    }
+
+    try {
+        await m.react('🕑');
+        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`);
+
+        let processedUrls = new Set();
+
+        for (let a of api.data.data) {
+            if (!processedUrls.has(a.url)) {
+                processedUrls.add(a.url);
+
+                // Verificación de si la URL es una imagen o un video
+                if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            image: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                } else {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            video: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                }
+            }
+        }
+        await m.react('✅'); 
+    } catch (error) {
+        console.log(error);
+        await m.react('❌');
+    }
+};
+
+handler.help = ['ig *<link>*'];
+handler.tags = ['dl'];
+handler.command = /^(ig|igdl|instagram)$/i;
+
+export default handler;
+
+
+
+
+
+/* import axios from 'axios';
+
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+    if (!args[0]) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Ingresa un link de Instagram`, m, fake);
+    }
+
     if (!args[0].match(new RegExp('^https?:\\/\\/(www\\.)?instagram\\.com\\/(p|tv|reel)\\/([a-zA-Z0-9_-]+)(\\/)?(\\?.*)?$'))) {
         await m.react('✖️');
         return conn.reply(m.chat, `☁️ Verifica que sea un link de Instagram`, m);
@@ -54,4 +117,4 @@ handler.help = ['ig *<link>*'];
 handler.tags = ['dl'];
 handler.command = /^(ig|igdl|instagram)$/i;
 
-export default handler;
+export default handler; */
