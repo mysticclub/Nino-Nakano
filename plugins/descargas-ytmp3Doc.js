219cf92184229a042933d0a1e8d62572e2da4ed2
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-26 23:52:25 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,80 +1,100 @@
-/* 
-- Search Happy Mod By Izumi-kzx
-- API Privada By Darkcore
-- Haz búsquedas en Happy Mod 
-- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
-*/
-import fetch from 'node-fetch'
-const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return m.reply('Ingresa el texto de lo que quieres buscar en HappyMod 🤍');
-  await m.react('🕓');
-
-  try {
-    async function createImage(url) {
-      const { imageMessage } = await generateWAMessageContent({image: { url }}, {upload: conn.waUploadToServer})
-      return imageMessage
-    }
-
-    let push = [];
-    let api = await fetch(`https://dark-core-api.vercel.app/api/happymodsearch?key=TWIzumi&text=${encodeURIComponent(text)}`);
-    let json = await api.json();
-
-    for (let app of json.results) {
-      let image = await createImage(app.image)
-
-      push.push({
-        body: proto.Message.InteractiveMessage.Body.fromObject({
-          text: `> ✐ Nombre » *${app.name.trim()}*\n> ✰ Valoración » *${app.stars}* ⭐ \n> 🜸 Link » _${app.link}_`
-        }),
-        footer: proto.Message.InteractiveMessage.Footer.fromObject({
-          text: ''
-        }),
-        header: proto.Message.InteractiveMessage.Header.fromObject({
-          title: '',
-          hasMediaAttachment: true,
-          imageMessage: image
-        }),
-        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-          buttons: [
-            {
-              "name": "cta_copy",
-              "buttonParamsJson": "{\"display_text\":\"🔗 Descargar APK\",\"id\":\"123456789\",\"copy_code\":\".download " + app.link + "\"}"
-            },
-          ]
-        })
-      });
-    }
-
-    const msg = generateWAMessageFromContent(m.chat, {
-      viewOnceMessage: {
-        message: {
-          messageContextInfo: {
-            deviceListMetadata: {},
-            deviceListMetadataVersion: 2
-          },
-          interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-            body: proto.Message.InteractiveMessage.Body.create({text: '*`\Resultados de:\`* ' + `${text}`}),
-            footer: proto.Message.InteractiveMessage.Footer.create({text: ''}),
-            header: proto.Message.InteractiveMessage.Header.create({hasMediaAttachment: false}),
-            carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({cards: [...push]})
-          })
+import ws from 'ws'
+import translate from 'translate-google-api'
+//const translate = require('translate-google-api');
+//const ws = require("ws");
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+class BlueArchive {
+        voice = async function voice(text, model = "Airi", speed = 1.2) {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    if (!text || text.length >= 500)
+                        throw new Error(`Make sure to enter valid text, that's not exceed 500 words!`);
+                    if (speed && (speed < 0.1 || speed > 2))
+                        speed = 2;
+                    model = "JP_" + model;
+                    const base_url = "https://ori-muchim-bluearchivetts.hf.space/";
+                    const session_hash = this.generateSession();
+                    const socket = new ws("wss://ori-muchim-bluearchivetts.hf.space/queue/join");
+                    socket.on("message", (data) => {
+                        const d = JSON.parse(data.toString("utf8"));
+                        switch (d.msg) {
+                            case "send_hash": {
+                                socket.send(JSON.stringify({
+                                    fn_index: 0,
+                                    session_hash,
+                                }));
+                                break;
+                            }
+                            case "send_data": {
+                                socket.send(JSON.stringify({
+                                    fn_index: 0,
+                                    session_hash,
+                                    data: [text, model, speed],
+                                }));
+                                break;
+                            }
+                            case "estimation":
+                            case "process_starts": {
+                                break;
+                            }
+                            case "process_completed": {
+                                const o = d.output;
+                                const name = o.data[1]?.name;
+                                socket.close();
+                                return resolve({
+                                    text,
+                                    model: model,
+                                    speed,
+                                    result: {
+                                        duration: +o.duration.toFixed(2),
+                                        path: name,
+                                        url: base_url + "file=" + name,
+                                    },
+                                });
+                            }
+                            default: {
+                                console.log(`Unexpected message type : ${data.toString("utf8")}`);
+                                break;
+                            }
+                        }
+                    });
+                } catch (e) {
+                    return reject(`Error in voice process: ${e.message}`);
+                }
+            });
         }
-      }
-    }, {
-      'quoted': m
-    });
-
-    await m.react('✅');
-    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-  } catch (error) {
-    console.error(error)
-  }
+        generateSession = function generateSession() {
+            return Math.random().toString(36).substring(2);
+        }
+    }
+    try {
+        let [teks, char, sped] = text.split('|')
+        if (!teks || !char) return m.reply('> Example: .ttsba jawa|momoi')
+        const suppVoice = ['airi', 'akane', 'akari', 'ako', 'aris', 'arona', 'aru', 'asuna', 'atsuko', 'ayane', 'azusa', 'cherino', 'chihiro', 'chinatsu', 'chise', 'eimi', 'erica', 'fubuki', 'fuuka', 'hanae', 'hanako', 'hare', 'haruka', 'haruna', 'hasumi', 'hibiki', 'hihumi', 'himari', 'hina', 'hinata', 'hiyori', 'hoshino', 'iori', 'iroha', 'izumi', 'izuna', 'juri', 'kaede', 'karin', 'kayoko', 'kazusa', 'kirino', 'koharu', 'kokona', 'kotama', 'kotori', 'main', 'maki', 'mari', 'marina', 'mashiro', 'michiru', 'midori', 'miku', 'mimori', 'misaki', 'miyako', 'miyu', 'moe', 'momoi', 'momoka', 'mutsuki', 'NP0013', 'natsu', 'neru', 'noa', 'nodoka', 'nonomi', 'pina', 'rin', 'saki', 'saori', 'saya', 'sena', 'serika', 'serina', 'shigure', 'shimiko', 'shiroko', 'shizuko', 'shun', 'ShunBaby', 'sora', 'sumire', 'suzumi', 'tomoe', 'tsubaki', 'tsurugi', 'ui', 'utaha', 'wakamo', 'yoshimi', 'yuuka', 'yuzu', 'zunko']
+        if (!suppVoice.includes(char)) {
+            const txtVoice = suppVoice.map(name => name.charAt(0).toUpperCase() + name.slice(1)).sort().map(name => `> - ${name}`).join('\n');
+            m.reply(
+`*Char tidak ditemukan!*
+Berikut list char yang disupport:
+${txtVoice}`
+            )
+            return
+        }
+        m.reply("Wait....")
+        const pedo = new BlueArchive()
+        const translated = await translate(teks, { to: 'ja', autoCorrect: false })
+        let charr = char.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1));
+        const ba = await pedo.voice(translated[0], charr, sped || 1)
+        conn.sendMessage(m.chat, { audio: { url: ba.result.url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
+    } catch (err) {
+        console.error(err);
+        m.reply('> Yahh, error...')
+    }
 }
-
-handler.help = ["happymodsearch *<texto>*"]
-handler.tags = ["search"]
-handler.command = /^(happymodsearch)$/i
-
+handler.help = ['ttsba']
+handler.tags = ['audio']
+handler.command = /^(ttsba)$/i
+handler.limit = true
+handler.register = true
 export default handler
+//module.exports = handler;
