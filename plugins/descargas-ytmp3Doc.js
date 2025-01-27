91a3b806c850a21c6f38acfea10a0f2c2113b7d6
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-27 00:16:17 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,105 +1,126 @@
-
-import ws from 'ws';
-import translate from 'translate-google-api';
-
-let handler = async (m, { conn, text }) => {
-    class BlueArchive {
-        voice = async function voice(texto, modelo = "Airi", velocidad = 1.2) {
-            return new Promise(async (resolve, reject) => {
-                try {
-                    if (!texto || texto.length >= 500)
-                        throw new Error("¡Asegúrate de ingresar un texto válido que no exceda los 500 caracteres!");
-                    if (velocidad && (velocidad < 0.1 || velocidad > 2))
-                        velocidad = 2;
-                    modelo = "JP_" + modelo;
-                    const base_url = "https://ori-muchim-bluearchivetts.hf.space/";
-                    const session_hash = this.generarSesion();
-                    const socket = new ws("wss://ori-muchim-bluearchivetts.hf.space/queue/join");
-                    socket.on("message", (data) => {
-                        const d = JSON.parse(data.toString("utf8"));
-                        switch (d.msg) {
-                            case "send_hash": {
-                                socket.send(JSON.stringify({
-                                    fn_index: 0,
-                                    session_hash,
-                                }));
-                                break;
-                            }
-                            case "send_data": {
-                                socket.send(JSON.stringify({
-                                    fn_index: 0,
-                                    session_hash,
-                                    data: [texto, modelo, velocidad],
-                                }));
-                                break;
-                            }
-                            case "estimation":
-                            case "process_starts": {
-                                break;
-                            }
-                            case "process_completed": {
-                                const o = d.output;
-                                const nombre = o.data[1]?.name;
-                                socket.close();
-                                return resolve({
-                                    texto,
-                                    modelo: modelo,
-                                    velocidad,
-                                    resultado: {
-                                        duracion: +o.duration.toFixed(2),
-                                        ruta: nombre,
-                                        url: base_url + "file=" + nombre,
-                                    },
-                                });
-                            }
-                            default: {
-                                console.log(`Mensaje inesperado: ${data.toString("utf8")}`);
-                                break;
-                            }
-                        }
-                    });
-                } catch (e) {
-                    return reject(`Error en el proceso de voz: ${e.message}`);
-                }
-            });
-        }
-        generarSesion = function generarSesion() {
-            return Math.random().toString(36).substring(2);
-        }
+import axios from 'axios';
+ 
+const handler = async (m, { conn, text, args, prefix, command }) => {
+  if (!args[0]) {
+    return m.reply(`Masukkan ID Game Free Fire.\n\nContoh:\n${prefix + command} 12345678`);
+  }
+ 
+  try {
+    await m.reply("_Sedang memproses data..._");
+ 
+    const url = `https://api.vreden.web.id/api/ffstalk?id=${args[0]}`;
+    const response = await axios.get(url);
+ 
+    if (!response.data || !response.data.result) {
+      return m.reply('Data tidak ditemukan atau ID yang dimasukkan salah.');
     }
-
-    try {
-        let [texto, personaje, velocidad] = text.split('|');
-        if (!texto || !personaje) return m.reply('> Ejemplo: .ttsba hola cómo estás|momoi');
-        const vocesSoportadas = ['airi', 'akane', 'akari', 'ako', 'aris', 'arona', 'aru', 'asuna', 'atsuko', 'ayane', 'azusa', 'cherino', 'chihiro', 'chinatsu', 'chise', 'eimi', 'erica', 'fubuki', 'fuuka', 'hanae', 'hanako', 'hare', 'haruka', 'haruna', 'hasumi', 'hibiki', 'hihumi', 'himari', 'hina', 'hinata', 'hiyori', 'hoshino', 'iori', 'iroha', 'izumi', 'izuna', 'juri', 'kaede', 'karin', 'kayoko', 'kazusa', 'kirino', 'koharu', 'kokona', 'kotama', 'kotori', 'main', 'maki', 'mari', 'marina', 'mashiro', 'michiru', 'midori', 'miku', 'mimori', 'misaki', 'miyako', 'miyu', 'moe', 'momoi', 'momoka', 'mutsuki', 'NP0013', 'natsu', 'neru', 'noa', 'nodoka', 'nonomi', 'pina', 'rin', 'saki', 'saori', 'saya', 'sena', 'serika', 'serina', 'shigure', 'shimiko', 'shiroko', 'shizuko', 'shun', 'ShunBaby', 'sora', 'sumire', 'suzumi', 'tomoe', 'tsubaki', 'tsurugi', 'ui', 'utaha', 'wakamo', 'yoshimi', 'yuuka', 'yuzu', 'zunko'];
-        if (!vocesSoportadas.includes(personaje.toLowerCase())) {
-            const listaVoces = vocesSoportadas.map(nombre => `> - ${nombre.charAt(0).toUpperCase() + nombre.slice(1)}`).join('\n');
-            m.reply(
-`*¡Personaje no encontrado!*
-Lista de personajes soportados:
-${listaVoces}`
-            );
-            return;
-        }
-        m.reply("Espera un momento...");
-        const blueArchive = new BlueArchive();
-        
-        // Traducir el texto al japonés antes de enviarlo al servicio
-        const textoTraducido = await translate(texto, { to: 'ja', autoCorrect: false });
-
-        const personajeFormateado = personaje.toLowerCase().split(' ').map(p => p.charAt(0).toUpperCase() + p.slice(1));
-        const resultado = await blueArchive.voice(textoTraducido[0], personajeFormateado, velocidad || 1);
-        conn.sendMessage(m.chat, { audio: { url: resultado.resultado.url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
-    } catch (err) {
-        console.error(err);
-        m.reply('> Oh no, ocurrió un error...');
-    }
+ 
+    console.log('API Response:', response.data);
+ 
+    const result = response.data.result;
+ 
+    const account = {
+      id: result?.account?.id || 'Tidak tersedia',
+      name: result?.account?.name || 'Tidak tersedia',
+      level: result?.account?.level || 'Tidak tersedia',
+      xp: result?.account?.xp || 'Tidak tersedia',
+      region: result?.account?.region || 'Tidak tersedia',
+      like: result?.account?.like || 'Tidak tersedia',
+      bio: result?.account?.bio || 'Tidak tersedia',
+      create_time: result?.account?.create_time || 'Tidak tersedia',
+      last_login: result?.account?.last_login || 'Tidak tersedia',
+      honor_score: result?.account?.honor_score || 'Tidak tersedia',
+      booyah_pass: result?.account?.booyah_pass || 'Tidak tersedia',
+      booyah_pass_badge: result?.account?.booyah_pass_badge || 'Tidak tersedia',
+      evo_access_badge: result?.account?.evo_access_badge || 'Tidak tersedia',
+      equipped_title: result?.account?.equipped_title || 'Tidak tersedia',
+      BR_points: result?.account?.BR_points || 'Tidak tersedia',
+      CS_points: result?.account?.CS_points || 'Tidak tersedia',
+    };
+ 
+    const petInfo = {
+      name: result?.pet_info?.name || 'Tidak tersedia',
+      level: result?.pet_info?.level || 'Tidak tersedia',
+      type: result?.pet_info?.type || 'Tidak tersedia',
+      xp: result?.pet_info?.xp || 'Tidak tersedia',
+    };
+ 
+    const guild = {
+      name: result?.guild?.name || 'Tidak tersedia',
+      id: result?.guild?.id || 'Tidak tersedia',
+      level: result?.guild?.level || 'Tidak tersedia',
+      member: result?.guild?.member || 'Tidak tersedia',
+      capacity: result?.guild?.capacity || 'Tidak tersedia',
+    };
+ 
+    const ketuaGuild = {
+      create_time: result?.ketua_guild?.create_time || 'Tidak tersedia',
+      last_login: result?.ketua_guild?.last_login || 'Tidak tersedia',
+      BP_bagdes: result?.ketua_guild?.BP_bagdes || 'Tidak tersedia',
+      BR_points: result?.ketua_guild?.BR_points || 'Tidak tersedia',
+      CS_points: result?.ketua_guild?.CS_points || 'Tidak tersedia',
+      level: result?.ketua_guild?.level || 'Tidak tersedia',
+      like: result?.ketua_guild?.like || 'Tidak tersedia',
+      name: result?.ketua_guild?.name || 'Tidak tersedia',
+      equipped_title: result?.ketua_guild?.equipped_title || 'Tidak tersedia',
+      id: result?.ketua_guild?.id || 'Tidak tersedia',
+      xp: result?.ketua_guild?.xp || 'Tidak tersedia',
+    };
+ 
+    
+    const resultText = `*[ STALK FF ]*\n\n` +
+      `> *Account Info:*\n` +
+      `  *ID*: ${account.id}\n` +
+      `  *Name*: ${account.name}\n` +
+      `  *Level*: ${account.level}\n` +
+      `  *XP*: ${account.xp}\n` +
+      `  *Region*: ${account.region}\n` +
+      `  *Like*: ${account.like}\n` +
+      `  *Bio*: ${account.bio}\n` +
+      `  *Created At*: ${account.create_time}\n` +
+      `  *Last Login*: ${account.last_login}\n` +
+      `  *Honor Score*: ${account.honor_score}\n` +
+      `  *Booyah Pass*: ${account.booyah_pass}\n` +
+      `  *Booyah Badge*: ${account.booyah_pass_badge}\n` +
+      `  *Evo Access Badge*: ${account.evo_access_badge}\n` +
+      `  *Equipped Title*: ${account.equipped_title}\n` +
+      `  *BR Points*: ${account.BR_points}\n` +
+      `  *CS Points*: ${account.CS_points}\n\n` +
+      `> *Pet Info:*\n` +
+      `  *Name*: ${petInfo.name}\n` +
+      `  *Level*: ${petInfo.level}\n` +
+      `  *Type*: ${petInfo.type}\n` +
+      `  *XP*: ${petInfo.xp}\n\n` +
+      `> *Guild Info:*\n` +
+      `  *Name*: ${guild.name}\n` +
+      `  *ID*: ${guild.id}\n` +
+      `  *Level*: ${guild.level}\n` +
+      `  *Members*: ${guild.member}\n` +
+      `  *Capacity*: ${guild.capacity}\n\n` +
+      `> *Guild Leader Info:*\n` +
+      `  *Created At*: ${ketuaGuild.create_time}\n` +
+      `  *Last Login*: ${ketuaGuild.last_login}\n` +
+      `  *BP Badge*: ${ketuaGuild.BP_bagdes}\n` +
+      `  *BR Points*: ${ketuaGuild.BR_points}\n` +
+      `  *CS Points*: ${ketuaGuild.CS_points}\n` +
+      `  *Level*: ${ketuaGuild.level}\n` +
+      `  *Like*: ${ketuaGuild.like}\n` +
+      `  *Name*: ${ketuaGuild.name}\n` +
+      `  *Equipped Title*: ${ketuaGuild.equipped_title}\n` +
+      `  *ID*: ${ketuaGuild.id}\n` +
+      `  *XP*: ${ketuaGuild.xp}`;
+ 
+    // Mengirimkan hasil ke pengguna
+    await conn.sendMessage(m.chat, { text: resultText, mentions: [m.sender] }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    m.reply('Terjadi kesalahan saat mengambil data.\n> Error: ' + error.message);
+  }
 };
-
-handler.help = ['ttsba'];
-handler.tags = ['audio'];
-handler.command = /^(ttsba)$/i;
+ 
+handler.command = ['ffstalk', 'stalkff', 'cekidff'];
+handler.help = ['ffstalk <id>', 'stalkff <id>', 'cekidff <id>'];
+handler.tags = ['tools', 'game'];
 handler.limit = true;
-handler.register = true;
-
+ 
 export default handler;
