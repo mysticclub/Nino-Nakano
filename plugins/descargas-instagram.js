83e388486b4f4d951a7bc8ea3bfdf9593999141d
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-26 20:22:54 -0400

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -43,7 +43,7 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
                 }
             }
         }
-        await m.react('✅'); 
+        await m.react('🍟'); 
     } catch (error) {
         console.log(error);
         await m.react('❌');
