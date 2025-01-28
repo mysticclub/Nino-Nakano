62bf19ece01052135c57d9dfc08c61b653dc74e5
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-28 16:48:31 -0500

    Update nsfw-xvideo.js

--- plugins/nsfw-xvideo.js
+++ plugins/nsfw-xvideo.js
@@ -38,6 +38,7 @@ const handler = async (m, { conn, text }) => {
 };
 
 handler.help = ['xvideo'];
+handler.tags = ['nsfw'];
 handler.command = ['xvideo', 'xvideodownload'];
 handler.register = true;
 
