ed0dc36be2375f2e2af5e05c7333c348e68e7640
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-29 00:12:53 -0400

    Actualizar tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -8,7 +8,7 @@ contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttributio
 title: packname,
 body: dev,
 previewType: 0, thumbnail: icons, 
-sourceUrl: channel }}})
+sourceUrl: canal }}})
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
 await m.react(done)
