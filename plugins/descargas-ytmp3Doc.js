8a1b34f8008c2f6575f8e3ae5d7de3fdc36a0e31
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-30 12:32:57 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 const handler = async (m, { conn, text }) => {
-if (!text) throw conn.reply(m.chat, '*Ingrese la url de la APK que quiera descargar.*', m);
+if (!text) return conn.reply(m.chat, '*Ingrese el nombre de la APK que quiera buscar.*', m);
 try {
 const apiUrl = `https://dark-core-api.vercel.app/api/download/getapk?key=user1&url=${encodeURIComponent(text)}`;
 const res = await fetch(apiUrl);
 const json = await res.json();
-if (!json.success) throw '*[❗] Error, no se encontraron resultados para su búsqueda.*';
+if (!json.success) return conn.reply(m.chat, '*[❗] Error, no se encontraron resultados para su búsqueda.*', m);
 const { title, version, category, downloadLink } = json.data;
 const caption = `🍟 *Descargador de APK* 🍟\n\n• *Nombre:* ${title}\n• *Versión:* ${version}\n• *Categoría:* ${category}`;
 await conn.sendMessage(m.chat, {
@@ -15,8 +15,10 @@ fileName: `${title}.apk`,
 caption
 }, { quoted: m });
 } catch (e) {
-throw '*[❗] Error al procesar la solicitud.*';
+return conn.reply(m.chat, '*[❗] Error al procesar la solicitud.*', m);
 }
 };
+handler.help = ['apkdroid *<url>*'];
+handler.tags = ['dl'];
 handler.command = /^(apkdroid)$/i;
 export default handler;
