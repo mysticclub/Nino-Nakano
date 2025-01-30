9458b5f5634c13677c5f1b38d847dd35d996b732
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-30 00:19:59 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -6,25 +6,6 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-if (!text) throw '• Ingresa un enlace de YouTube.'
-try {
-let res = await fetch(`https://api.diioffc.web.id/api/download/ytmp3?url=${encodeURIComponent(text)}`)
-let json = await res.json()
-if (json.status && json.result?.download?.url) {
-let { title, thumbnail, views, duration, author, download } = json.result
-let caption = `• *Título:* ${title}\n• *Canal:* ${author.name}\n• *Duración:* ${duration.timestamp}\n• *Vistas:* ${views.toLocaleString()}`
-await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption }, { quoted: m })
-await conn.sendMessage(m.chat, { audio: { url: download.url }, mimetype: 'audio/mpeg', fileName: download.filename || 'audio.mp3' }, { quoted: m })
-} else throw 'No se pudo obtener el audio.'
-} catch (e) {
-m.reply(`❌ *Error:* Ocurrió un error desconocido`)
-}}
-handler.command = ['ytmp3']
-export default handler
-
-/* import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '• Ingresa un enlace de YouTube.', m)
 
 try {
@@ -46,4 +27,4 @@ conn.reply(m.chat, '❌ Error al descargar el video.', m)
 }}
 
 handler.command = ['ytmp7']
-export default handler */
+export default handler
