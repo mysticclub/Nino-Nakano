e736c79458d805063c4b7f6a07abdec086e6e067
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-31 14:21:24 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -48,7 +48,6 @@ export default handler; */
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-  await m.react('✖️');
   if (!text) throw `Proporcióname el enlace de YouTube para que pueda ayudarte. 🎵`;
 
   await m.react('🕓');
