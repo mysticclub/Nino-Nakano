24b56fdfeb7c99ad036ca4c1163f11c205d01c50
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-31 13:41:53 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,25 +1,16 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return conn.reply(m.chat, `🍟 Ingresa un texto para hablar con Lori Ai`, m)
-  }
-
-  try {
-    let api = await fetch(`https://api.davidcyriltech.my.id/ai/lori?text=${encodeURIComponent(text)}`)
-    let json = await api.json()
-    
-    if (json.success) {
-      await m.reply(json.response)
-    } else {
-      await m.reply(`🍟 Hubo un error al obtener la respuesta.`)
-    }
-  } catch (error) {
-    console.error(error)
-    await m.reply(`🍟 Ocurrió un error al procesar tu solicitud.`)
-  }
-}
-
+if (!text) return conn.reply(m.chat, `🍟 Ingresa un texto para hablar con Lori Ai`, m)
+try {
+let api = await fetch(`https://api.davidcyriltech.my.id/ai/lori?text=${encodeURIComponent(text)}`)
+let json = await api.json()
+if (json.success) await m.reply(json.response)
+else await m.reply(`🍟 Hubo un error al obtener la respuesta.`)
+} catch (error) {
+console.error(error)
+await m.reply(`🍟 Ocurrió un error al procesar tu solicitud.`)
+}}
 handler.command = /^(loriai)$/i
 
 export default handler
