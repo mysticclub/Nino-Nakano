691095fa4653623d89a48eb97e0f4032214def3a
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-01 20:31:42 -0400

    Actualizar descargas-ytmp4Doc.js

---
+++ plugins/descargas-ytmp4Doc.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, isBotAdmin, isGroup, botname }) => {
+    if (!isGroup) return m.reply('❌ Este comando solo puede usarse en grupos.');
+    if (!isBotAdmin) return m.reply('🤖 Necesito ser administrador para obtener el enlace del grupo.');
+
+    let response = await conn.groupInviteCode(m.chat);
+    let groupLink = `https://chat.whatsapp.com/${response}`;
+
+    await conn.sendMessage(m.chat, { 
+        location: { 
+            degreesLatitude: 0, 
+            degreesLongitude: 0, 
+            name: "Klik/Salin Link Grup", 
+            address: `© ${botname}`, 
+            url: groupLink
+        }
+    }, { quoted: m });
+};
+
+handler.command = ['linkgc', 'linkgroup', 'linkgrup'];
+
+export default handler;
