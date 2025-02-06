fb48613fe6a7b6bc7dd9e1b42234a06f5b5c0b07
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-06 11:31:20 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -301,19 +301,10 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-  return [d, ' D ', h, ' H ', m, ' M '].map(v => v.toString().padStart(2, '0')).join('');
-}
-function clockStringP(ms) {
-  let ye = isNaN(ms) ? '--' : Math.floor(ms / 31104000000) % 10
-  let mo = isNaN(ms) ? '--' : Math.floor(ms / 2592000000) % 12
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) % 30
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [ye, ' *Years 🗓️*\n', mo, ' *Month 🌙*\n', d, ' *Days ☀️*\n', h, ' *Hours 🕐*\n', m, ' *Minute ⏰*\n', s, ' *Second ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
+  let d = isNaN(ms) ? '00' : Math.floor(ms / 86400000); // Días
+  let h = isNaN(ms) ? '00' : Math.floor(ms / 3600000) % 24; // Horas
+  let m = isNaN(ms) ? '00' : Math.floor(ms / 60000) % 60; // Minutos
+  return [d, h, m].map(v => v.toString().padStart(2, '0')).join(':');
 }
 function ucapan() {
     const time = moment.tz('America/Buenos_Aires').format('HH')
