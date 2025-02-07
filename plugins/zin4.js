aaab9137040ef8c9f1528a8f41fb63dc35810443
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-07 14:52:03 -0400

    Actualizar zin4.js

--- plugins/zin4.js
+++ plugins/zin4.js
@@ -1,140 +1,76 @@
 const { prepareWAMessageMedia, generateWAMessageFromContent } = (await import('@whiskeysockets/baileys')).default;
 const { randomBytes } = await import("crypto");
 
-let texting = 'q pasa w'
+let texting = 'q pasa w';
 
 conn.sendMessage(m.chat, {
-
-image: { url: 'https://files.catbox.moe/brjxwz.jpg'},
-
-caption: texting,
-
-footer: 'la cosa es seria chavito\nte amo w',
-
-contextInfo: {
-
-mentionedJid: [m.sender],
-
-forwardingScore: 999,
-
-isForwarded: true,
-
-externalAdReply: {
-
-showAdAttribution: true,
-
-title: Sock Ai,
-
-body: "like your pussycat",
-
-sourceUrl: "https://instagram.com/c4rl@s_9e",
-
-thumbnailUrl: 'https://files.catbox.moe/brjxwz.jpg',
-
-mediaType: 1,
-
-renderLarger Thumbnail: false
-
-}},
-
-buttons: [
-
-{
-
-buttonId: '.ping',
-
-buttonText: {
-
-displayText: 'ping'
-
-},
-
-type: 1,
-
-},
-
-{
-
-buttonId: '.tqto',
-
-buttonText: {
-
-displayText: 'tqto'
-
-},
-
-type: 1,
-
-},
-
-{
-
-type: 4,
-
-nativeFlowInfo: {
-
-name: 'single_select',
-
-params]son: JSON.stringify({
-
-title: 'Dont click',
-
-sections: [
-
-{
-
-title: 'my focking bicht',
-
-highlight_label: "",
-
-rows: [
-
-{
-
-header: Message',
-
-title: 'love dog',
-
-description: i like pussydog
-
-id: ".
-
-},
-
-{
-
-header: Message',
-
-title: 'check ping',
-
-description: i like pussycat",
-
-id: ",
-
-},
-
-1,
-
-},
-
-1,
-
-}),
-
-},
-
-},
-
-1,
-
-header Type: 1,
-
-viewOnce: true
-
-})
-
-handler.command = ['test']
-export default handler 
+    image: { url: 'https://files.catbox.moe/brjxwz.jpg' },
+    caption: texting,
+    footer: 'la cosa es seria chavito\nte amo w',
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        externalAdReply: {
+            showAdAttribution: true,
+            title: 'Sock Ai',
+            body: "like your pussycat",
+            sourceUrl: "https://instagram.com/c4rl@s_9e",
+            thumbnailUrl: 'https://files.catbox.moe/brjxwz.jpg',
+            mediaType: 1,
+            renderLargerThumbnail: false
+        }
+    },
+    buttons: [
+        {
+            buttonId: '.ping',
+            buttonText: {
+                displayText: 'ping'
+            },
+            type: 1,
+        },
+        {
+            buttonId: '.tqto',
+            buttonText: {
+                displayText: 'tqto'
+            },
+            type: 1,
+        },
+        {
+            type: 4,
+            nativeFlowInfo: {
+                name: 'single_select',
+                paramsJson: JSON.stringify({
+                    title: 'Dont click',
+                    sections: [
+                        {
+                            title: 'my focking bicht',
+                            highlight_label: "",
+                            rows: [
+                                {
+                                    header: 'Message',
+                                    title: 'love dog',
+                                    description: 'i like pussydog',
+                                    id: "."
+                                },
+                                {
+                                    header: 'Message',
+                                    title: 'check ping',
+                                    description: 'i like pussycat',
+                                    id: ","
+                                }
+                            ]
+                        }
+                    ]
+                })
+            }
+        }
+    ],
+    headerType: 1
+});
+
+handler.command = ['test'];
+export default handler;
 
 
 /* let handler = async (m, { conn, args, usedPrefix, command }) => {
