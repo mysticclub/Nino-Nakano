0611f4603c121000ddaca4bf1074c490465ab4e8
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-07 14:48:19 -0400

    Actualizar zin4.js

--- plugins/zin4.js
+++ plugins/zin4.js
@@ -1,91 +1,138 @@
 const { prepareWAMessageMedia, generateWAMessageFromContent } = (await import('@whiskeysockets/baileys')).default;
 const { randomBytes } = await import("crypto");
 
-const handler = async (m, { conn }) => {
-    try {
-        let img = 'https://files.catbox.moe/iejoer.jpg'; // Imagen principal
-        let thumbnail = 'https://cdn.arifzyn.site/f/sy6tjbzk.jpg'; // Imagen en el link superior
-        let botname = global.botname || "MiBot"; // Asegurar que `global.botname` esté definido
+let texting = q pasa w
 
-        await m.react('🤍');
+conn.sendMessage(m.cht, {
 
-        // Crear mensaje con todo incluido
-        const message = generateWAMessageFromContent(m.chat, {
-            viewOnceMessage: {
-                message: {
-                    interactiveMessage: {
-                        body: { text: `Selamat datang kak *${m.pushName}*` }, // Mensaje de bienvenida
-                        footer: `─ Waktu: *Selamat Subuh*\n─ Runtime: *3 hours, 7 minutes, 19 seconds*`,
-                        contextInfo: {
-                            mentionedJid: [m.sender],
-                            forwardingScore: 999,
-                            isForwarded: true,
-                            externalAdReply: {
-                                showAdAttribution: true,
-                                title: `PokPok`,
-                                body: "Thezy - Chan",
-                                thumbnailUrl: thumbnail,
-                                sourceUrl: "https://whatsapp.com/channel/0029VawsCnQ9mrGkOuburC1z",
-                                mediaType: 1,
-                                renderLargerThumbnail: false,
-                            },
-                        },
-                        nativeFlowMessage: {
-                            name: 'single_select',
-                            paramsJson: JSON.stringify({
-                                title: 'Klick Hare!!',
-                                sections: [
-                                    {
-                                        title: 'Thezy X Fauzialifatah',
-                                        highlight_label: '',
-                                        rows: [
-                                            {
-                                                header: '⌬ Message Owner',
-                                                title: '└ Menampilkan Menu Owner',
-                                                description: botname,
-                                                id: randomBytes(6).toString("hex"),
-                                            },
-                                            {
-                                                header: '⌬ Message Group',
-                                                title: '└ Menampilkan Menu Owner',
-                                                description: botname,
-                                                id: randomBytes(6).toString("hex"),
-                                            },
-                                        ],
-                                    },
-                                ],
-                            }),
-                        },
-                        buttons: [
-                            {
-                                buttonId: '.tes',
-                                buttonText: { displayText: 'Owner Botz' },
-                                type: 1,
-                            },
-                            {
-                                buttonId: '.thxto',
-                                buttonText: { displayText: 'Supporter' },
-                                type: 1,
-                            },
-                        ],
-                        header: {
-                            imageMessage: (await prepareWAMessageMedia({ image: { url: img } }, { upload: conn.waUploadToServer })).imageMessage,
-                        },
-                    },
-                },
-            },
-        }, {});
+image: { url: 'https://files.catbox.moe/brjxwz.jpg'},
 
-        // Enviar mensaje con todo en un solo envío
-        await conn.relayMessage(m.chat, message.message, { messageId: message.key.id });
+caption: texting,
 
-    } catch (e) {
-        console.error(e);
-    }
-};
+footer: la cosa es seria chavito\nte amo w`,
 
-handler.command = ['test'];
-export default handler;
+contextInfo: {
+
+mentionedJid: [m.sender],
+
+forwardingScore: 999,
+
+isForwarded: true,
+
+externalAdReply: {
+
+showAdAttribution: true,
+
+title: Sock Ai,
+
+body: "like your pussycat",
+
+sourceUrl: "https://instagram.com/c4rl@s_9e",
+
+thumbnailUrl: 'https://files.catbox.moe/brjxwz.jpg',
+
+mediaType: 1,
+
+renderLarger Thumbnail: false
+
+}},
+
+buttons: [
+
+{
+
+buttonId: '.ping',
+
+buttonText: {
+
+displayText: 'ping'
+
+},
+
+type: 1,
+
+},
+
+{
+
+buttonId: '.tqto',
+
+buttonText: {
+
+displayText: 'tqto'
+
+},
+
+type: 1,
+
+},
+
+{
+
+type: 4,
+
+nativeFlowInfo: {
+
+name: 'single_select',
+
+params]son: JSON.stringify({
+
+title: 'Dont click',
+
+sections: [
+
+{
+
+title: 'my focking bicht',
+
+highlight_label: "",
+
+rows: [
+
+{
+
+header: Message',
+
+title: 'love dog',
+
+description: i like pussydog
+
+id: ".
+
+},
+
+{
+
+header: Message',
+
+title: 'check ping',
+
+description: i like pussycat",
+
+id: ",
+
+},
+
+1,
+
+},
+
+1,
+
+}),
+
+},
+
+},
+
+1,
+
+header Type: 1,
+
+viewOnce: true
+
+})
+
 
 
 /* let handler = async (m, { conn, args, usedPrefix, command }) => {
