5776c660dd8182ecd393cf5450c34da250b330d1
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-07 10:17:43 -0400

    Actualizar zin4.js

--- plugins/zin4.js
+++ plugins/zin4.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   conn.sendMessage(m.chat, {
     image: { url: 'https://files.catbox.moe/iejoer.jpg' },
     caption: dev,
-    footer: `─ Waktu: *${ucapanWaktu}*\n─ Runtime: *${runtime(process.uptime())}*`,
+    footer: `─ Waktu*\n─ Runtime: *${runtime(process.uptime())}*`,
     contextInfo: {
       mentionedJid: [m.sender, owned],
       forwardingScore: 999,
