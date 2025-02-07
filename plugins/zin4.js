d34a247168b4360cb33c38f71e211112ef892cdd
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-07 10:08:12 -0400

    Actualizar zin4.js

--- plugins/zin4.js
+++ plugins/zin4.js
@@ -1,21 +1,31 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let txt = `Nose?`
-conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
-  {
-    buttonId: ".menu", 
-    buttonText: { 
-      displayText: 'test' 
-    }
-  }, {
-    buttonId: ".s", 
-    buttonText: {
-      displayText: "Hola"
-    }
-  }
-],
-  viewOnce: true,
-  headerType: 1,
-}, { quoted: m })
+  let img = [ 
+    'https://i.ibb.co/YDGYRhx/file.jpg'
+  ]
+
+    await m.react('🤍')
+    await conn.sendMessage(m.chat, {
+      image: { url: img },
+      caption: estilo(text),
+      footer: dev,
+      buttons: [
+        {
+          buttonId: `.ping`,
+          buttonText: {
+            displayText: 'ᯓᡣ𐭩 ⍴іᥒg',
+          },
+        },
+        {
+          buttonId: `.owner`,
+          buttonText: {
+            displayText: 'ᯓᡣ𐭩 ᥆ᥕᥒᥱr',
+          },
+        },
+      ],
+      viewOnce: true,
+      headerType: 4,
+    }, { quoted: m })
 }
 handler.command = ['test']
 export default handler
