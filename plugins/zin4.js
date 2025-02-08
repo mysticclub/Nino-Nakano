7f3a70d8cf5a13baaf7721e44b50cdd85fac6d10
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-07 23:30:35 -0400

    Actualizar zin4.js

--- plugins/zin4.js
+++ plugins/zin4.js
@@ -2,6 +2,102 @@ import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysoc
 
 const handler = async (m, { conn }) => {
     await conn.sendMessage(m.chat, {
+        image: { url: 'https://files.catbox.moe/brjxwz.jpg' },
+        caption: 'i like y',
+        footer: 'la cosa es seria chavito\nte amo w',
+        contextInfo: {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true
+        },
+        buttons: [
+            {
+                buttonId: '.ping',
+                buttonText: { displayText: 'ping' },
+                type: 1,
+            },
+            {
+                buttonId: '.tqto',
+                buttonText: { displayText: 'tqto' },
+                type: 1,
+            },
+            {
+                type: 4,
+                nativeFlowInfo: {
+                    name: 'single_select',
+                    paramsJson: JSON.stringify({
+                        title: 'Dont click',
+                        sections: [
+                            {
+                                title: 'my focking bicht',
+                                highlight_label: '',
+                                rows: [
+                                    {
+                                        header: '⌬ Message',
+                                        title: 'menu all',
+                                        description: 'i like pussydog',
+                                        id: ".menu"
+                                    },
+                                    {
+                                        header: '⌬ Message',
+                                        title: 'check ping',
+                                        description: 'i like pussycat',
+                                        id: ".ping"
+                                    },
+                                ],
+                            },
+                        ],
+                    }),
+                },
+            },
+            {
+                type: 4,
+                nativeFlowInfo: {
+                    name: 'single_select',
+                    paramsJson: JSON.stringify({
+                        title: 'Another List',
+                        sections: [
+                            {
+                                title: 'Second List',
+                                highlight_label: '',
+                                rows: [
+                                    {
+                                        header: '⌬ Message',
+                                        title: 'status bot',
+                                        description: 'check bot status',
+                                        id: ".status"
+                                    },
+                                    {
+                                        header: '⌬ Message',
+                                        title: 'about us',
+                                        description: 'info about the bot',
+                                        id: ".about"
+                                    },
+                                ],
+                            },
+                        ],
+                    }),
+                },
+            },
+        ],
+        headerType: 1,
+        viewOnce: true
+    }, { quoted: m }); // Agrega el mensaje citado
+};
+
+handler.command = ['tesyt'];
+export default handler;
+
+
+
+
+
+
+
+/* import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+
+const handler = async (m, { conn }) => {
+    await conn.sendMessage(m.chat, {
         image: { url: 'https://i.ibb.co/JDswhPq/file.jpg' },
         caption: 'i like y',
         footer: 'la cosa es seria chavito\nte amo w',
@@ -57,4 +153,4 @@ const handler = async (m, { conn }) => {
 };
 
 handler.command = ['tesyt'];
-export default handler;
+export default handler; */
