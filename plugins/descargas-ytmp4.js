6bd0bc1c01b67d0a90f0fb79af397bfa8a99ec96
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-02-09 18:21:15 -0500

    Update descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,14 +1,20 @@
 let handler = async (m, { conn, text }) => {
     await m.react('🕓');
+    
     if (!text) return conn.reply(m.chat, "❌ *Por favor, ingresa un enlace de YouTube.*", m);
 
     try {
         let url = `https://ytcdn.project-rian.my.id/download?url=${encodeURIComponent(text)}&resolution=360`;
+    
+        await conn.sendMessage(m.chat, { 
+            video: { url }, 
+            caption: "🎥 *Aquí está tu video*" 
+        }, { quoted: m });
 
-        await conn.sendMessage(m.chat, { video: { url }, caption: "🎥 *Aquí está tu video*" }, { quoted: m });
         await m.react('✅');
-    } catch (e) {
-        console.error(e);
+    } catch {
+        await m.react('❌');
+        conn.reply(m.chat, "*Hubo un error en la api*", m);
     }
 };
 
