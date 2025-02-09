71d321a2178af8d8abc36546bf604811b1bfcbff
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-09 14:38:36 -0400

    Actualizar descargas-ytmp3Doc.js

--- plugins/descargas-ytmp3Doc.js
+++ plugins/descargas-ytmp3Doc.js
@@ -1,29 +1,29 @@
-/* HTML WEB By WillZek 
-- Free Codes Titan
-- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
-*/
-
-// [🕵️] 𝗛𝗧𝗠𝗟 𝗪𝗘𝗕
-
 import fetch from 'node-fetch';
 
-let handler = async(m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, `🍟 Ingresa un link de APK desde APKFab.`, m);
+    await m.react('🕓');
 
-if (!args[0]) return m.reply('🍭 Ingresa Un Link De Alguna Web');
-m.react('🕑');
+    try {
+        let api = await fetch(`https://bk9.fun/download/apkfab?url=${encodeURIComponent(text)}`);
+        let json = await api.json();
+        if (!json.status) return m.reply('❌ Error al obtener los detalles del enlace, por favor intenta nuevamente.');
 
-let api = `https://delirius-apiofc.vercel.app/tools/htmlextract?url=${args[0]}`;
-let titan = await fetch(api);
-let json = await titan.json();
-let data = json.html;
+        let { title, link, size } = json.BK9;
+        let caption = `*「✐」${title}*\n\n> ❒ Tamaño » *${size || 'Desconocido'}*\n> 🔗 [Descargar APK](${link})`;
 
-let xd = 'https://files.catbox.moe/trd8vu.jpg';
-let html = `*🎩 HTML EXTRAIDO DE LA WEB:* ${data}*`
+        // Enviar el archivo APK con el caption
+        await conn.sendFile(m.chat, link, title, caption, m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true });
 
-m.react('✅');
-conn.sendMessage(m.chat, { image: { url: xd }, caption: html }, { quoted: fkontak });
-};
+        await m.react('✅');
+    } catch (error) {
+        console.error(error);
+        m.reply('❌ Ocurrió un error al procesar la solicitud.');
+    }
+}
 
-handler.command = ['htmlweb', 'hweb'];
+handler.help = ['apkfab *<url>*'];
+handler.tags = ['dl'];
+handler.command = ['apkfab'];
 
 export default handler;
