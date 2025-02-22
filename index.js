59d257e0ea99542070ad6918186ebfa7df46eace
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-22 13:22:38 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? [`Genesis-Ultra`, 'Edge', '20.0.04'] : methodCodeQR ? [`Genesis-Ultra`, 'Chrome', '20.0.04'] : ['Ubuntu', 'Chrome', '20.0.04'], 
+browser: opcion == '1' ? [`Genesis-Ultra`, 'Chrome', '20.0.04'] : methodCodeQR ? [`Genesis-Ultra`, 'Chrome', '20.0.04'] : ['Ubuntu', 'Chrome', '20.0.04'], 
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
