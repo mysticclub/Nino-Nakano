27ad4135be461b16947c7aa1dae069093d708607
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-02 07:50:49 -0400

    Actualizar descargas-ytmp4Doc.js

---
+++ plugins/descargas-ytmp4Doc.js
@@ -0,0 +1,33 @@
+import { googleImage } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let user = global.db.data.users[m.sender]
+
+    if (!text) throw `𝗤𝘂𝗲 𝗯𝘂𝘀𝗰𝗮𝗿? 🤔️\n𝗨𝘀𝗲𝗹𝗼 𝗱𝗲 𝗹𝗮 𝘀𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 𝗺𝗮𝗻𝗲𝗿𝗮\n𝗘𝗷𝗲𝗺𝗽𝗹𝗼:\n*${usedPrefix + command} Loli*`
+
+    const res = await googleImage(text)
+    let image = res.getRandom()
+    let link = image
+
+    await delay(1000)
+
+    await conn.sendMessage(m.chat, { 
+        image: { url: link }, 
+        caption: `*🔎 Resultado De: ${text}*`, 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `${usedPrefix + command} ${text}`,
+                buttonText: { displayText: 'Siguiente' }
+            }
+        ],
+        viewOnce: true,
+        headerType: 4
+    }, { quoted: m })
+}
+
+handler.help = ['imagen *<texto>*']
+handler.tags = ['internet', 'dl']
+handler.command = /^(image2|imagen2)$/i
+
+export default handler
