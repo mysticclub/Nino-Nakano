2963163192120f520c361a7fbd2942777c74dc8a
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-03 23:47:34 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -2,31 +2,35 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
     if (!args[0]) return conn.reply(m.chat, '⚠️ Proporciona un enlace de YouTube.', m);
-    await m.react('⏳');
+    await m.react('🕒');
+    
     let url = args[0];
-    let api = `https://api.agungny.my.id/api/youtube-audio?url=${encodeURIComponent(url)}`;
+    let api = `https://dark-core-api.vercel.app/api/download/YTMP3?key=api&url=${encodeURIComponent(url)}`;
 
     try {
         let res = await fetch(api);
         let json = await res.json();
 
-        if (!json.status || !json.result) {
+        if (!json.status || !json.download) {
             return conn.reply(m.chat, '❌ No se pudo obtener el audio.', m);
         }
 
-        let { title, url_audio } = json.result;
+        let { title, download } = json;
 
         await conn.sendMessage(m.chat, { 
-            audio: { url: url_audio }, 
+            audio: { url: download }, 
             mimetype: 'audio/mp4', 
             fileName: `${title}.mp3` 
         }, { quoted: m });
-      await m.react('✅');
+
+        await m.react('✅');
     } catch (e) {
-     await m.react('❌');
+        await m.react('❌');
     }
 };
-handler.help = ['ytmp3 *<url>*']
-handler.tags = ['dl']
+
+handler.help = ['ytmp3 *<url>*'];
+handler.tags = ['dl'];
 handler.command = /^(ytmp3|yta)$/i;
+
 export default handler;
