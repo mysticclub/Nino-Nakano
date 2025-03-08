2af66f68281fee6d1ddc96391dad01be3e481019
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-07 23:34:22 -0400

    Add files via upload

---
+++ plugins/Main•allmenu.js
@@ -0,0 +1,207 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
+try {	
+let vn = './media/menu.mp3'
+let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+let name = await conn.getName(m.sender)
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+})
+let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+}).format(d)
+let time = d.toLocaleTimeString(locale, {
+hour: 'numeric',
+minute: 'numeric',
+second: 'numeric'
+})
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) {
+process.send('uptime')
+_muptime = await new Promise(resolve => {
+process.once('message', resolve)
+setTimeout(resolve, 1000)
+}) * 1000
+}
+let { money, joincount } = global.db.data.users[m.sender]
+let user = global.db.data.users[m.sender]
+let muptime = clockString(_muptime)
+let uptime = clockString(_uptime)
+let totalreg = Object.keys(global.db.data.users).length
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+let replace = {
+'%': '%',
+p: _p, uptime, muptime,
+me: conn.getName(conn.user.jid),
+npmname: _package.name,
+npmdesc: _package.description,
+version: _package.version,
+exp: exp - min,
+maxexp: xp,
+totalexp: exp,
+xp4levelup: max - exp,
+github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+readmore: readMore
+}
+text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+//let user = global.db.data.users[m.sender]
+//user.registered = false
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+let username = conn.getName(who)
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+//let enlace = { contextInfo: { externalAdReply: {title: wm, body: 'support group' , sourceUrl: nna, thumbnail: await(await fetch(img)).buffer() }}}
+  let pp = './Menu2.jpg'
+//let pp = gataVidMenu.getRandom()
+/*await conn.sendMessage(m.chat, {
+        text: `*Hey @${m.sender.split`@`[0]} cargando el menu..`,
+        contextInfo: { 
+          mentionedJid: [m.sender],
+        }
+      }, { quoted: m })*/
+  
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+let menu = `  
+*•/• Ai Otho - MD •/•*
+ 
+> 👤 Creador : Daniel 🇦🇱
+> 🌍 Pais : Perú 🇵🇪
+> ❄️ Web : https://danxxl-emer.vercel.app
+> 🍟 Terminal : Linux
+> ☁️ Librería : Baileys
+
+*•/• Información Bot •/•*
+
+☁️.owner
+☁️.totalfunciones
+☁️.velocidad 
+☁️.sistema 
+☁️.uptime
+
+*•/• Downloaders •/•*
+
+🍄.facebook
+🍄.ytmp3 
+🍄.ytmp4
+🍄.tiktok
+🍄.tiktokimg
+🍄.Spotifydl
+🍄.applemusicdl
+🍄.clouddl
+🍄.pinterestdl
+🍄.Instagram
+🍄.applemusic 
+🍄.souncloud
+🍄.apk
+
+
+*•/• Searchs •/•*
+
+🎋.spotifysearch
+🎋.mercadolibre
+🎋.wikisearch
+🎋.google
+🎋.tiktokvid
+🎋.shazam
+🎋.yts
+🎋.pinterest
+🎋.tiktoksearch
+🎋.tiktokvid
+🎋.twittersearch
+🎋.applemusicsearch
+🎋.cloudsearch
+🎋.npmjssearct
+🎋.imagenes
+
+*•/• Al •/•*
+
+🍘.ia
+🍘.gemini
+🍘.bing
+🍘@ai
+
+*•/• Tools •/•*
+
+🍧.ssweb
+🍧.hd
+🍧.reenviar
+🍧.ver
+🍧.poll
+🍧.tourl
+🍧.ibb
+🍧.toimg
+🍧.topvideo
+🍧.topgifaud
+🍧.topmp3
+
+*•/• Groups  •/•*
+
+🌼.enable
+🌼.disable
+🌼.unbanchat
+🌼.banchat
+🌼.promote
+🌼.demote
+🌼.delete
+🌼.tagall
+🌼.tag
+🌼.kick
+🌼.mute
+
+*•/• RPG  •/•*
+
+🌵.claim
+🌵.dulces
+🌵.crimen
+🌵.minar
+🌵.work
+🌵.verificar
+🌵.perfil
+
+*•/• Stickers •/•*
+
+🍥.sticker
+🍥.qc
+🍥.wm`.trim()
+//conn.sendFile(m.chat, pp, 'lp.jpg', menu, m, false, { contextInfo: { mentionedJid }})
+let img = await (await fetch(`https://i.ibb.co/10xZ4YZ/file.jpg`)).buffer()  
+conn.sendFile(m.chat, img, 'lp.jpg', menu, m, false, { contextInfo: { mentionedJid }})
+await m.react('✅')	
+} catch (e) {
+await m.reply(`❌️ Ocurrió un error.\n\n` + e)
+await m.react(error)
+}}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['main']
+handler.command = /^(menu|allmenu\?)$/i
+//handler.register = true
+handler.exp = 50
+handler.fail = null
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
