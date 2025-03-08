759b96f47466179c306ed00afb93510ad9653b28
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-08 14:47:38 -0400

    Actualizar Stalk•kawai.js

--- plugins/Stalk•kawai.js
+++ plugins/Stalk•kawai.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) {
-    return conn.reply(m.chat, `🚩 Por favor, ingrese un nombre de usuario para buscar.\n\nEjemplo:\n> *${usedPrefix + command}* Vegeta_Sola`, m);
+    return conn.reply(m.chat, `🧇 Por favor, ingrese un nombre de usuario para buscar.\n\nEjemplo:\n> *${usedPrefix + command}* Vegeta_Sola`, m);
   }
 
   await m.react('🕓');
