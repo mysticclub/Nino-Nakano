bdf4fa23a1dae40e04c32f27d3f7c58c2b714e9c
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-09 18:24:42 -0400

    Crear Test•1.js

---
+++ plugins/Test•1.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn, text, command }) => {
+  if (m.isGroup) {
+    return await m.reply('Este comando debe ser enviado en privado.');
+  }
+
+  let [commandText, groupLink] = text.split(" ");
+  if (!groupLink) {
+    return await m.reply('Debes proporcionar el enlace del grupo después del comando. Ejemplo: .salir <enlace del grupo>');
+  }
+
+  try {
+    let group = await conn.groupMetadata(groupLink);
+    if (!group || !group.participants.some(p => p.id === conn.user.id)) {
+      return await m.reply('No soy miembro del grupo al que intentas que me retire. No puedo realizar la acción.');
+    }
+
+    await conn.reply(groupLink, `🍟 *Ai Genesis* Abandona El Grupo, Fue Genial Estar Aquí`);
+    await conn.groupLeave(groupLink);
+
+    await conn.reply(m.chat, 'Orden cumplida, me retiro del grupo ✅', m);
+
+  } catch (e) {
+    console.log(e);
+    await m.reply('Ocurrió un error al intentar salir del grupo.');
+  }
+}
+
+handler.command = ['salir']
+handler.private = true
+handler.rowner = true
+export default handler;
