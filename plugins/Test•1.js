51b54f6ec26e641f0e8becf2fa45b2787053e81f
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-09 18:24:55 -0400

    Actualizar Test•1.js

--- plugins/Test•1.js
+++ plugins/Test•1.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, text, command }) => {
   }
 }
 
-handler.command = ['salir']
+handler.command = ['salir2']
 handler.private = true
 handler.rowner = true
 export default handler;
