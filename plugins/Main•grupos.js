9f298a1a8d80bc322c55588d77aa6fb30f59e517
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-10 20:55:16 -0400

    Actualizar Main•grupos.js

--- plugins/Main•grupos.js
+++ plugins/Main•grupos.js
@@ -1,27 +1,28 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://i.ibb.co/YDGYRhx/file.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D* 🍂
-
-1- Ai Otho 🍭
-*✰* ${group}
+1- Génesis Ultra ☁️
+*✰* https://chat.whatsapp.com/GqKwwoV2JJaJDP2SL7SddX
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
 
 ➠ Enlace anulado? entre aquí! 
 
-♡ Canal :
-*✰* ${canal}
+♡ Canal Genesis :
+*✰* https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
+
+♡ Canal TK-Host :
+*✰* https://whatsapp.com/channel/0029VaGGynJLY6d43krQYR2g
 
 > ${dev}`
-
-await conn.sendFile(m.chat, imagen3, "otho.jpg", grupos, fkontak, null, rcanal)
-
-await m.react(emojis)
-
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, fake)
 }
 handler.help = ['grupos']
 handler.tags = ['main']
-handler.command = ['grupos', 'aigrupos', 'gruposai']
+handler.command = /^(grupos)$/i
 export default handler
