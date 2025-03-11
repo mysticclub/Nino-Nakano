2ee1caf4bc6e01c36a65944bbb6cbd9b18efea70
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-10 21:13:32 -0400

    Actualizar Config•autodetect.js

--- plugins/Config•autodetect.js
+++ plugins/Config•autodetect.js
@@ -8,13 +8,13 @@ let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://qu.ax/QGAVS.jpg'  
 
 let nombre, foto, edit, newlink, status, admingp, noadmingp
-nombre = `*${usuario}*\n✨️ Ha cambiado el nombre del grupo\n\n🌻 Ahora el grupo se llama:\n*${m.messageStubParameters[0]}*`
+nombre = `> ✐ \`ALERTA\` » ${usuario} ha *Ha cambiado el nombre del grupo* a *${m.messageStubParameters[0]}* ✐`
 foto = `*${usuario}*\n🚩 Ha cambiado la imagen del grupo`
 edit = `*${usuario}*\n🌺 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo`
 newlink = `🌸 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*`
 status = `El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n💬 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
-admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🥳\n\n💫 Acción hecha por:\n*» ${usuario}*`
-noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 😿\n\n💫 Acción hecha por:\n*» ${usuario}*`
+admingp = `> ✐ \`ALERTA\` » ${usuario} ha *promovido* a @${m.messageStubParameters[0].split`@`[0]} ✐`
+noadmingp =  `> ✐ \`ALERTA\` » ${usuario} ha *degradado* a @${m.messageStubParameters[0].split`@`[0]} ✐`
 
 if (chat.detect && m.messageStubType == 21) {
 await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })   
