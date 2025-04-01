7fb52953119000ead14544cb9378b8148432d5bf
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-01 06:24:43 -0500

    Crear buscador-cuevanasearch.js

---
+++ plugins/buscador-cuevanasearch.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+if (!text) return m.reply(`${emoji} Ingresa el nombre de una pelicula\n> *Ejemplo: /cuevana Deadpool*.`)
+
+try {
+let api = await fetch(`https://delirius-apiofc.vercel.app/search/cuevana?q=${encodeURIComponent(text)}`)
+let json = await api.json()
+
+let JT = '📽️ Cuevana  -  Search 📽️';
+json.data.forEach((app, index) => {
+      JT += `\n\n═══════════════════════`;
+      JT += `\n☁️ *Nro :* ${index + 1}`
+      JT += `\n🖼️ *Imagen:* ${app.image}`
+      JT += `\n⚜️ *Titulo:* ${app.title}`
+      JT += `\n📚 *Descripcion:* ${app.description}`
+      JT += `\n🔗 *Link:* ${app.link}`
+}) 
+
+m.reply(JT)
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['cuevanasearch', 'cuevana']
+
+export default handler
+handler.group = true
