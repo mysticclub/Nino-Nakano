b5d927dfa879ce754540ec23c9a875923cd6ab99
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-01 15:31:39 -0500

    Crear buscador-imagen.js

---
+++ plugins/buscador-imagen.js
@@ -0,0 +1,18 @@
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa un término de búsqueda.`, m);
+await m.react(rwait)
+conn.reply(m.chat, '🍭 Descargando su imagen, espere un momento...', m)
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `${emoji} Resultado de ${text}`, '⪛✰ Imagen - Búsqueda ✰⪜', null, messages, m);
+};
+handler.help = ['imagen'];
+handler.tags = ['buscador', 'tools', 'descargas'];
+handler.command = ['image', 'imagen'];
+handler.group = true;
+
+export default handler;
