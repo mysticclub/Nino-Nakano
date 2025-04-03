18cdbb486246ed97bccaf79b43b8387b41dbc84a
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-03 17:20:51 -0500

    Crear fun-formarpareja.js

---
+++ plugins/fun-formarpareja.js
@@ -0,0 +1,17 @@
+const toM = (a) => '@' + a.split('@')[0];
+function handler(m, {groupMetadata}) {
+  const ps = groupMetadata.participants.map((v) => v.id);
+  const a = ps.getRandom();
+  let b;
+  do b = ps.getRandom();
+  while (b === a);
+  m.reply(`*${toM(a)}, Deberías casarte 💍 con ${toM(b)}, hacen una bonita pareja 💓*`, null, {
+    mentions: [a, b],
+  });
+}
+handler.help = ['formarpareja'];
+handler.tags = ['fun'];
+handler.command = ['formarpareja', 'formarparejas'];
+handler.group = true;
+
+export default handler;
