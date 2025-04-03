2d6603b9f931d5384a5349e499768098cfcd915b
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-03 17:24:31 -0500

    Crear fun-memes.js

---
+++ plugins/fun-memes.js
@@ -0,0 +1,12 @@
+import hispamemes from 'hispamemes'
+let handler = async (m, { conn, usedPrefix, command }) => {
+const meme = hispamemes.meme()
+conn.sendFile(m.chat, meme, '', '', fkontak)
+m.react(emoji2)
+}
+handler.help = ['meme']
+handler.tags = ['fun']
+handler.command = ['meme', 'memes']
+handler.group = true;
+
+export default handler
