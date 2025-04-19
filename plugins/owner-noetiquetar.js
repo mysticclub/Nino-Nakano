e369e94e9ac3c228885d5e3f4c5dac3d4bd5b9ed
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-19 08:41:24 -0500

    Actualizar owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,13 +1,1 @@
-//COdigo por Zaphkiel Ender
-
-
-
-let handler = async (m, {conn, text}) => {
-  let name = await conn.getName(m.sender);
-
-conn.reply(m.chat, `> [ 💖 ️] No etiquetes a mi owner, si es algo urgente contacta con el a su chat privado para mas informacion` ,m, { contextInfo:{ externalAdReply: {title: '❤️‍🔥 ༆ᴺᵉˣᵘˢ乂club숬', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/51920227615'}}})
-}
-handler.customPrefix = /@51920227615/i;
-handler.command = new RegExp();
-
-export default handler;
+//nada :v
